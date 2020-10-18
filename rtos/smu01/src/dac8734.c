5675268924d9d627386c35f58f927f53a568d9a1
julian1 <git@julian1.io>
2020-10-18 14:11:46 +1100

    update

--- rtos/smu01/src/dac8734.c
+++ rtos/smu01/src/dac8734.c
@@ -284,7 +284,7 @@ void dac_reset(void)
   */
   uart_printf("mcu gpio read %d %d\n\r", gpio_get(DAC_PORT, DAC_GPIO0), gpio_get(DAC_PORT, DAC_GPIO1));
 
-  // ok this hangs perhaps - because there is nothing connected
+  // ok this appears to hang if dac is not populated
   uart_printf("dac clearing dac register\n\r");
   dac_write_register1( 0);
   uart_printf("mcu gpio read %d %d\n\r", gpio_get(DAC_PORT, DAC_GPIO0), gpio_get(DAC_PORT, DAC_GPIO1));
