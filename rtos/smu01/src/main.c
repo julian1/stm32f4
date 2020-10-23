9d2eddd1786746fbb30b1602fe5ed17f182c6320
julian1 <mail@julian1.io>
2020-10-23 19:20:09 +1100

    update

--- rtos/smu01/src/main.c
+++ rtos/smu01/src/main.c
@@ -455,12 +455,12 @@ static void adc_test2(void)
 
   // integrate up
   gpio_set(ADC_PORT, ADC_IN_CTL | ADC_MUX_AGND_CTL);    // turn off agnd in
-  gpio_clear(ADC_PORT, ADC_IN_CTL | ADC_MUX_VFB_CTL);                // turn on VFB ref
+  gpio_clear(ADC_PORT, ADC_IN_CTL | ADC_MUX_VFB_CTL);    // turn on VFB ref
   gpio_set(ADC_PORT, ADC_RESET_CTL);                    // clear the short of the cap - start integrating
   task_sleep(3);
 
   // integrate down
-  gpio_set(ADC_PORT, ADC_MUX_VFB_CTL);                  // turn off VFB ref
+  gpio_set(ADC_PORT, ADC_IN_CTL | ADC_MUX_VFB_CTL);    // turn off VFB ref
   gpio_clear(ADC_PORT, ADC_REFP10V_CTL);                  // turn on P10V ref
   task_sleep(3);
 
