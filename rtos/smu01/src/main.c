8e046e90206ea0b1e142567f3283db15feae242b
julian1 <mail@julian1.io>
2020-10-23 19:15:32 +1100

    integrate vfb. but is curved?

--- rtos/smu01/src/main.c
+++ rtos/smu01/src/main.c
@@ -449,16 +449,15 @@ static void adc_test2(void)
   // return;
   uart_printf("adc test\n\r");
 
-  // dac_write_register(0x01, (1 << 12) );  // select monitor dac0  // works
-  // dac_write_register(0x01, (1 << 11) );     // ain, which is wired to vref65  // works
+  // dac_write_register(0x01, (1 << 12) );      // select monitor dac0  // works
+  // dac_write_register(0x01, (1 << 11) );      // ain, which is wired to vref65  // works
   // gpio_clear(ADC_PORT, ADC_MUX_DAC_VMON_CTL);
 
-  // integrates up
+  // integrate up
   gpio_set(ADC_PORT, ADC_IN_CTL | ADC_MUX_AGND_CTL);    // turn off agnd in
-  gpio_clear(ADC_PORT, ADC_MUX_VFB_CTL);                    // turn on N10V ref
+  gpio_clear(ADC_PORT, ADC_IN_CTL | ADC_MUX_VFB_CTL);                // turn on VFB ref
   gpio_set(ADC_PORT, ADC_RESET_CTL);                    // clear the short of the cap - start integrating
   task_sleep(3);
-
 
   // integrate down
   gpio_set(ADC_PORT, ADC_MUX_VFB_CTL);                  // turn off VFB ref
@@ -466,7 +465,7 @@ static void adc_test2(void)
   task_sleep(3);
 
   // reset
-  gpio_set(ADC_PORT, ADC_REFP10V_CTL);                  // turn off N10V ref
+  gpio_set(ADC_PORT, ADC_IN_CTL | ADC_REFP10V_CTL);                  // turn off N10V ref
   gpio_clear(ADC_PORT, ADC_IN_CTL | ADC_MUX_AGND_CTL);  // turn on agnd in
   gpio_clear(ADC_PORT, ADC_RESET_CTL);                    // stop integrating
 
