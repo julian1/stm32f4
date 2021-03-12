7c4a6c5397b520f6cc0ed1cc7fcd7b917e0bb9c7
julian1 <mail@julian1.io>
2021-03-12 17:27:09 +1100

    comment
    comment
    
    update
    
    update
    
    update

--- lib2/src/buffer.c
+++ lib2/src/buffer.c
@@ -1,7 +1,31 @@
 
+/*
+  Actually better approach is just,
+    leave interrupt unenabled, until item is written, then reinit the interrupt.
+
+    this doesn't get and put at the same time but ought to be enough. 
+    also write the value before updating the index.
+    so index will not just jump.
+
+  --------------
+  - issue - an interupt interupted by same priority interupt.  eg. get() on top of get()
+      so
+
+      see, https://www.eevblog.com/forum/embedded-computing/concurrency-in-a-bare-metal-environment/msg3506550/?topicseen#msg3506550
+
+  - need mutex.
+
+ 43   circular buffer
+ 44     - can an interupt be interrupted by a same priority interrupt.
+ 45       this is where the update of the index, and the write of the value can go wrong.
+*/
+
+
 #include "buffer.h"
 
+// TODO rename write() to put(), read() to get(), or even push() and pop()
 
+// float buffer...
 
 void fBufInit(FBuf *a, float *p, size_t sz)
 {
@@ -13,6 +37,7 @@ void fBufInit(FBuf *a, float *p, size_t sz)
 
 void fBufWrite(FBuf *a, float val)
 {
+  // update val, then increment index
   (a->p)[ a->wi] = val;
   a->wi = (a->wi + 1) % a->sz;
 }
@@ -20,6 +45,7 @@ void fBufWrite(FBuf *a, float val)
 
 bool fBufEmpty(FBuf *a)
 {
+  // TODO change name IsEmpty
   return a->ri == a->wi;
 }
 
@@ -32,13 +58,14 @@ float fBufRead(FBuf *a)
 
   float ret = (a->p)[a->ri];
   a->ri = (a->ri + 1) % a->sz;
+
   return ret;
 }
 
 ////////////////////
 
 
-// ring buffer for output...
+// char buffer
 
 
 void cBufInit(CBuf *a, char *p, size_t sz)
@@ -57,12 +84,14 @@ void cBufWrite(CBuf *a, char val)
 
 int32_t cBufRead(CBuf *a)
 {
+  // sentinal value...
   if(a->ri == a->wi)
     return -1;
 
+  // read then update index.
   char ret = (a->p)[ a->ri];
-
   a->ri = (a->ri + 1) % a->sz;
+
   return ret;
 }
 
