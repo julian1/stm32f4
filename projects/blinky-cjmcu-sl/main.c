2200f2d6f266871357aff3291689ee03f2829681
julian1 <git@julian1.io>
2021-03-28 17:35:46 +1100

    add blinky-cjmcu-sl

---
+++ projects/blinky-cjmcu-sl/main.c
@@ -0,0 +1,231 @@
+
+// cjmcu  stm32f407.
+// issue. is that board/stlink doesn't appear to reset cleanly. needs sleep.
+
+
+#include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/gpio.h>
+
+#include <libopencm3/cm3/nvic.h>
+#include <libopencm3/cm3/systick.h>
+
+#include <libopencm3/stm32/exti.h>
+
+#include <libopencm3/stm32/timer.h>
+
+// #include <libopencm3/cm3/scb.h>
+
+#include <stddef.h> // size_t
+//#include <math.h> // nanf
+//#include <stdio.h>
+
+
+#include "buffer.h"
+#include "miniprintf2.h"
+#include "usart2.h"
+#include "util.h"
+
+
+
+
+
+#define LED_PORT      GPIOE
+#define LED_OUT       GPIO0
+
+
+
+static void led_setup(void)
+{
+  gpio_mode_setup(LED_PORT, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, LED_OUT);
+}
+
+
+
+////////////////////////////////////////////////////////
+
+/* Common function descriptions */
+// #include "clock.h"
+
+/* milliseconds since boot */
+static volatile uint32_t system_millis;
+
+
+
+// static void clock_setup(void)
+static void clock_setup(uint32_t tick_divider)
+{
+  // TODO change name systick_setup().
+  // TODO pass clock reload divider as argument, to localize.
+
+  /* clock rate / 168000 to get 1mS interrupt rate */
+  // systick_set_reload(168000);
+  systick_set_reload(tick_divider);
+  systick_set_clocksource(STK_CSR_CLKSOURCE_AHB);
+  systick_counter_enable();
+
+  /* this done last */
+  systick_interrupt_enable();
+}
+
+
+////////////////////////////////////////////////////////
+
+// implement critical_error_blink() msleep() and usart_printf()
+// eg. rather than ioc/ dependency injection, just implement
+
+void critical_error_blink(void)
+{
+  // avoid passing arguments, consuming stack.
+	for (;;) {
+		gpio_toggle(LED_PORT,LED_OUT);
+		for(uint32_t i = 0; i < 500000; ++i)
+       __asm__("nop");
+	}
+}
+
+// defined in util.h
+void msleep(uint32_t delay)
+{
+  // TODO. review. don't think this works on wrap around
+  uint32_t wake = system_millis + delay;
+  if(wake < delay) {
+
+    // wait for overflow
+    while (system_millis > (UINT32_MAX / 2));
+  }
+  while (system_millis < wake);
+}
+
+
+
+
+static char buf1[1000];
+static char buf2[1000];
+
+static CBuf console_in;
+static CBuf console_out;
+
+
+
+
+void usart_printf( const char *format, ... )
+{
+  // TODO rename to just printf... it's not the responsibiilty of user to know context
+  // can override the write, to do flush etc.
+	va_list args;
+
+	va_start(args,format);
+	internal_vprintf((void *)cBufPut, &console_out, format,args);
+	va_end(args);
+}
+
+void flush( void)
+{
+  usart_sync_flush();
+}
+
+
+
+
+void sys_tick_handler(void)
+{
+  /*
+    this is an interupt. not sure how much work should do here.
+    albeit maybe its ok as dispatch point.
+
+    it will interrupt other stuff...
+  */
+  // equivalent to a rtos software timer
+  // we are in an interupt  context here... so don't do anything
+  // NOTE. we could actually set a flag.  or a vector of functions to call..
+  // and then process in main loop.
+  // eg. where things can be properly sequenced
+
+  system_millis++;
+
+
+  // 100ms.
+  if( system_millis % 100 == 0) {
+  }
+
+
+  // 500ms.
+  if( system_millis % 500 == 0) {
+    // blink led
+    gpio_toggle(LED_PORT, LED_OUT);
+  }
+
+}
+
+
+static void loop(void)
+{
+
+  while(true) {
+
+    // EXTREME - can actually call update at any time, in a yield()...
+    // so long as we wrap calls with a mechanism to avoid stack reentrancy
+    // led_update(); in systick.
+
+
+    // pump usart queues
+    usart_input_update();
+    usart_output_update();
+  }
+
+}
+
+
+
+
+int main(void)
+{
+  // high speed internal!!!
+  // this appears to work... 1Hz clock goes to 5Hz.
+  // although should be able to go to 100MHz.
+  // rcc_clock_setup_pll(&rcc_hsi_configs[RCC_CLOCK_3V3_84MHZ ]);
+
+  clock_setup(16000);
+
+
+
+  rcc_periph_clock_enable(RCC_SYSCFG); // maybe required for external interupts?
+
+
+  // LED
+  rcc_periph_clock_enable(RCC_GPIOE);
+  rcc_periph_clock_enable(RCC_GPIOD);
+  rcc_periph_clock_enable(RCC_GPIOA);
+
+  led_setup();
+
+
+  gpio_set(LED_PORT, LED_OUT);
+
+  // USART
+  // rcc_periph_clock_enable(RCC_GPIOA);
+  rcc_periph_clock_enable(RCC_GPIOB); // F410
+  rcc_periph_clock_enable(RCC_USART1);
+
+  cBufInit(&console_in, buf1, sizeof(buf1));
+  cBufInit(&console_out, buf2, sizeof(buf2));
+  usart_setup(&console_in, &console_out);
+
+
+  ////////////////////
+
+
+  usart_printf("\n--------\n");
+  usart_printf("starting\n");
+  // usart_printf("size %d\n", sizeof(fbuf) / sizeof(float));
+
+
+  loop();
+
+	for (;;);
+	return 0;
+}
+
+
+
+
