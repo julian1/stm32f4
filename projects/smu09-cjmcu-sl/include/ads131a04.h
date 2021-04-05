07137773d0c676e9c2d3027fe608bee2348b2584
julian1 <mail@julian1.io>
2021-04-06 09:13:04 +1000

    update

--- projects/smu09-cjmcu-sl/include/ads131a04.h
+++ projects/smu09-cjmcu-sl/include/ads131a04.h
@@ -4,27 +4,21 @@
 struct FBuf;
 
 /*
-  Actually. I think we should just about do this.
-  likewise for dac.
-  saves having to pull global defines from everywhere.
-  //////////
-  prefixing all the calls with ads131a04 becomes a bit messy.
-
-  its actually only two vars.
+  prefixing all funcs with ads131a04 is too messy.
+  could use a struct, albeit its only two vars we need to deal with.
 
   struct {
     uint32_t  spi;
     uint8_t   reg;
   }
- 
-  io_set(a->reg, ADC_RST); 
-  io_clear(a->reg, ADC_RST); 
-
 */
+
+
 extern void spi_adc_setup(uint32_t spi);
 
 int adc_init( uint32_t spi, uint8_t reg);
 
-// void adc_setup_spi( void );
+
+// OK. this thing is mcu? interrupt handling
 // void adc_exti_setup(FBuf *buffer1);
 
