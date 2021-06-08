b03b1fcdb50f4558c4facc61f3c6fcfe6953ff11
julian1 <mail@julian1.io>
2021-06-08 18:53:20 +1000

    comx relay switches

--- projects/smu11/src/main.c
+++ projects/smu11/src/main.c
@@ -322,14 +322,16 @@ static void update_soft_500ms(uint32_t spi  /*, state */)
 
 
 
-#if 0
-  // if(count % 5 == 0) {
+#if 1
+  if(count % 5 == 0) 
+  {
     // io_toggle(spi, INA_VFB_SW_REGISTER, INA_VFB_SW1_CTL | INA_VFB_SW2_CTL | INA_VFB_SW3_CTL);
     // io_toggle(spi, INA_DIFF_SW_REGISTER, INA_DIFF_SW1_CTL | INA_DIFF_SW2_CTL);
+    // io_toggle(spi, INA_ISENSE_SW_REGISTER,   ISENSE_SW1_CTL | ISENSE_SW2_CTL | ISENSE_SW3_CTL);
+    // io_toggle(spi, INA_IFB_SW1_CTL_REGISTER, INA_IFB_SW1_CTL | INA_IFB_SW2_CTL | INA_IFB_SW3_CTL);
 
-  {
-    io_toggle(spi, INA_ISENSE_SW_REGISTER,   ISENSE_SW1_CTL | ISENSE_SW2_CTL | ISENSE_SW3_CTL);
-    io_toggle(spi, INA_IFB_SW1_CTL_REGISTER, INA_IFB_SW1_CTL | INA_IFB_SW2_CTL | INA_IFB_SW3_CTL);
+    io_toggle(spi, RELAY_COM_REGISTER, RELAY_COM_X);
+
   }
 #endif
 
