eb0a6d4ecae1d90bc1f2870a1dcf04c70716349b
julian1 <mail@julian1.io>
2021-07-01 15:11:01 +1000

    update

--- lib2/include/bits.h
+++ lib2/include/bits.h
@@ -12,7 +12,12 @@
 
 #define GETFIELD(data, width, offset)         ((data) >> (offset)) & MASK(width)
 
-
+// change name uint_format_bits()
 extern char * uint_to_bits(char *buf, size_t width, uint32_t value);
+
+
+#define ARRAY_SIZE(x) (sizeof(x) / sizeof((x)[0]))
+
+
 
 
