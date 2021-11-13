83b92f51bc8eb266aa322f8731854c4c3e787013
julian1 <git@julian1.io>
2021-11-14 07:49:17 +1100

    comment

--- projects/smu12/include/spi-ice40.h
+++ projects/smu12/include/spi-ice40.h
@@ -2,6 +2,9 @@
 
 extern void spi_ice40_setup(uint32_t spi);
 
+// these don't have spi- prefix,  but it makes dealing with ice40 registers more clear.
+// should possibly move to separate file
+
 extern void ice40_reg_set( uint32_t spi, uint8_t r, uint8_t v);
 extern void ice40_reg_clear( uint32_t spi, uint8_t r, uint8_t v);
 extern void ice40_reg_write( uint32_t spi, uint8_t r, uint8_t v);
