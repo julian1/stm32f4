58d4a471da808a1a9306a8c9ead83e89bded8fa9
julian1 <mail@julian1.io>
2021-12-15 11:04:37 +1100

    factor code

---
+++ projects/blinky-stm32f407/include/fsmc-ssd1963.h
@@ -0,0 +1,15 @@
+
+
+void tft_gpio_init(void);
+
+void fsmc_setup(void);
+
+void LCD_TouchReg(uint8_t LCD_Reg);
+
+uint16_t LCD_ReadReg(uint8_t LCD_Reg);
+
+void LCD_WriteReg(uint8_t LCD_Reg, uint16_t LCD_RegValue);
+
+
+
+
--- projects/blinky-stm32f407/src/main.c
+++ projects/blinky-stm32f407/src/main.c
@@ -47,6 +47,7 @@
 
 #include "str.h"  //format_bits
 
+#include "fsmc-ssd1963.h"
 
 typedef struct app_t
 {
@@ -125,205 +126,6 @@ static char buf_console_out[1000];
 
 
 static app_t app;
-
-
-
-static void tft_gpio_init(void)
-{
-  /*
-    OK. first goal should be to read and write a register using fsmc and bus .
-    probably want the reset also.
-
-    see. kicad5/projects/control-panel-2/notes.tx
-  /projects/fsmc-tests/doc/andy.txt
-  */
-
-  // clocks are external
-  // SHOULD PUT ALL TFT stuff in header... or at least predeclare.
-  // parallel tft / ssd1963
-  // rcc_periph_clock_enable(RCC_GPIOD);
-  // rcc_periph_clock_enable(RCC_GPIOE);
-
-
-  #define TFT_GPIO_PORT       GPIOE
-  #define TFT_LED_A           GPIO2
-  #define TFT_REST            GPIO1
-
-  gpio_mode_setup(TFT_GPIO_PORT, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, TFT_LED_A | TFT_REST);
-  gpio_set( TFT_GPIO_PORT, TFT_LED_A ); // turn on backlight. works!!!
-
-
-  usart_printf("pull reset lo\n");
-  // reset. pull lo then high.
-  gpio_clear( TFT_GPIO_PORT, TFT_REST);
-  msleep(1000);
-  usart_printf("pull reset hi\n");
-  gpio_set( TFT_GPIO_PORT, TFT_REST);
-  msleep(1000);
-
-}
-
-
-
-
-
-
-
-#include <libopencm3/stm32/fsmc.h>
-
-
-
-static void fsmc_setup(void)
-{
-
-  /*
-    https://titanwolf.org/Network/Articles/Article?AID=198f4410-66a4-4bee-a263-bfbb244dbc45
-  */
-
- /* Enable PORTD and PORTE */
-  rcc_periph_clock_enable(RCC_GPIOD);
-
-  rcc_periph_clock_enable(RCC_GPIOE);
-
-
- /* Enable FSMC */
-  rcc_periph_clock_enable(RCC_FSMC);
-
-
-
-
- /* config FSMC data lines */
-
-  uint16_t portd_gpios = GPIO0 | GPIO1 | GPIO8 | GPIO9 | GPIO10 | GPIO14 | GPIO15;
-  // gpio_set_mode(GPIOD, GPIO_MODE_OUTPUT_2_MHZ, GPIO_CNF_OUTPUT_ALTFN_PUSHPULL, portd_gpios);
-  gpio_mode_setup(GPIOD, GPIO_MODE_AF, GPIO_PUPD_NONE, portd_gpios);
-  gpio_set_output_options(GPIOD, GPIO_OTYPE_PP, GPIO_OSPEED_25MHZ, portd_gpios);
-  gpio_set_af(GPIOD, GPIO_AF12, portd_gpios);
-
-
-
-  uint16_t porte_gpios = GPIO7 | GPIO8 | GPIO9 | GPIO10 | GPIO11 | GPIO12 | GPIO13 | GPIO14 | GPIO15;
-  // gpio_set_mode(GPIOE, GPIO_MODE_OUTPUT_2_MHZ, GPIO_CNF_OUTPUT_ALTFN_PUSHPULL, porte_gpios);
-  gpio_mode_setup(GPIOE, GPIO_MODE_AF, GPIO_PUPD_NONE, porte_gpios);
-  gpio_set_output_options(GPIOE, GPIO_OTYPE_PP, GPIO_OSPEED_25MHZ, porte_gpios);
-  gpio_set_af(GPIOE, GPIO_AF12, porte_gpios);
-
-
-  // these could be consolidated...
-
- /* config FSMC NOE */
-  // gpio_set_mode(GPIOD, GPIO_MODE_OUTPUT_2_MHZ, GPIO_CNF_OUTPUT_ALTFN_PUSHPULL, GPIO4);
-  gpio_mode_setup(GPIOD, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO4);
-  gpio_set_output_options(GPIOD, GPIO_OTYPE_PP, GPIO_OSPEED_25MHZ, GPIO4);
-  gpio_set_af(GPIOD, GPIO_AF12, GPIO4);
-
-
- /* config FSMC NWE */
-  // gpio_set_mode(GPIOD, GPIO_MODE_OUTPUT_2_MHZ, GPIO_CNF_OUTPUT_ALTFN_PUSHPULL, GPIO5);
-  gpio_mode_setup(GPIOD, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO5);
-  gpio_set_output_options(GPIOD, GPIO_OTYPE_PP, GPIO_OSPEED_25MHZ, GPIO5);
-  gpio_set_af(GPIOD, GPIO_AF12, GPIO5);
-
-
- /* config FSMC NE1 */
-  /* JA PD7 CS */
-  // gpio_set_mode(GPIOD, GPIO_MODE_OUTPUT_2_MHZ, GPIO_CNF_OUTPUT_ALTFN_PUSHPULL, GPIO7);
-  gpio_mode_setup(GPIOD, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO7);
-  gpio_set_output_options(GPIOD, GPIO_OTYPE_PP, GPIO_OSPEED_25MHZ, GPIO7);
-  gpio_set_af(GPIOD, GPIO_AF12, GPIO7);
-
-
-
- /* config FSMC A16 for D/C (select Data/Command ) */
-  /* JA PD11  RS */
-  // gpio_set_mode(GPIOD, GPIO_MODE_OUTPUT_2_MHZ, GPIO_CNF_OUTPUT_ALTFN_PUSHPULL, GPIO11);
-  gpio_mode_setup(GPIOD, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO11);
-  gpio_set_output_options(GPIOD, GPIO_OTYPE_PP, GPIO_OSPEED_25MHZ, GPIO11);
-  gpio_set_af(GPIOD, GPIO_AF12, GPIO11);
-
-
-
-#if 0
-#endif
-
-
- /* config FSMC register */
-  FSMC_BTR(0) = FSMC_BTR_ACCMODx(FSMC_BTx_ACCMOD_B) |
-                FSMC_BTR_DATLATx(0)  |
-                FSMC_BTR_CLKDIVx(0)  |
-                // FSMC_BTR_CLKDIVx(16)  |
-                FSMC_BTR_BUSTURNx(0) |
-                FSMC_BTR_DATASTx(5)  |
-                FSMC_BTR_ADDHLDx(0)  |
-                FSMC_BTR_ADDSETx(1);
-
-
-  FSMC_BCR(0) = FSMC_BCR_WREN | FSMC_BCR_MWID | FSMC_BCR_MBKEN;
-
-}
-
-// JA
-#define __IO volatile
-
-/*
-  __IO flag appears undefined.
-*/
- typedef struct
- {
-
-  __IO uint16_t LCD_REG;
-
-  __IO uint16_t LCD_RAM;
- } LCD_TypeDef;
-
-// #define LCD_BASE    ((uint32_t)(0x60000000 | 0x00020000 -2 ) )
-// JA
-#define LCD_BASE    ((uint32_t)(0x60000000 | (0x00020000 -2) ) )
-
-/* JA
-  >  (0x60000000 | (0x00020000 -2)).toString(16)
-  "6001fffe"
-  > (0x60000000 | (0x00020000 )).toString(16)
-  "60020000"
-*/
-
-/*
-When you access A16 becomes zero in the LCD-> LCD_REG. (Address 0x6001FFFE)
-When you access A16 is 1 in LCD-> LCD_RAM. (Address 0x60020000)
-*/
-
-#define LCD         ((LCD_TypeDef *) LCD_BASE)
-
-
-#if 0
-
-static uint16_t LCD_ReadRAM(void)
- {
-
-  /* Write 16-bit Index (then Read Reg) */
-
-  // LCD->LCD_REG = R34 /* Select GRAM Reg */
-
-  // JA
-  LCD->LCD_REG = 0x0A ; /* Select GRAM Reg */
-                          // 0x0A get power mode....
-                          // hang on. these are commands... that accept parameters...
-
-  /* Read 16-bit Reg */
-
-  return LCD->LCD_RAM;
- }
-#endif
-
-
-static void LCD_TouchReg(uint8_t LCD_Reg);
-
-static uint16_t LCD_ReadReg(uint8_t LCD_Reg);
-
-static void LCD_WriteReg(uint8_t LCD_Reg, uint16_t LCD_RegValue);
-
-
-
 
 
 
@@ -454,19 +256,22 @@ int main(void)
   // LCD_ReadReg( 0x00 );  // nop
 
   while(1) {
-    // reg = 0x0A;
-    reg = 0x0D;   // == 11
-    x = LCD_ReadReg( reg );
-    usart_printf("reg %u (%02x)  r %u  %s\n", reg,  reg, x, format_bits(buf, 16, x));
-    msleep(1000);
+
+    // also see read_ddb. a lot of serial stuff. 
 
     reg = 0x0A;   // == 1000
     x = LCD_ReadReg( reg );
     usart_printf("reg %u (%02x)  r %u  %s\n", reg,  reg, x, format_bits(buf, 16, x));
     msleep(1000);
 
+    // reg = 0x0A;
+    reg = 0x0D;   // == 11
+    x = LCD_ReadReg( reg );
+    usart_printf("reg %u (%02x)  r %u  %s\n", reg,  reg, x, format_bits(buf, 16, x));
+    msleep(1000);
 
 
+  // wrong....
     reg = 0x26;   // == 1000
     x = LCD_ReadReg( reg );
     usart_printf("reg %u (%02x)  r %u  %s\n", reg,  reg, x, format_bits(buf, 16, x));
@@ -644,37 +449,5 @@ should try a loop for the same address...
 
 // put the code after the function. to prevent inlining.
 
-
-
-static void LCD_TouchReg(uint8_t LCD_Reg)
-{
-  /* Write 16-bit Index (then Read Reg) */
-  LCD->LCD_REG = LCD_Reg;
-
-
-}
-
-static uint16_t LCD_ReadReg(uint8_t LCD_Reg)
- {
-
-  /* Write 16-bit Index (then Read Reg) */
-  LCD->LCD_REG = LCD_Reg;
-
-  msleep(1);
-
-  /* Read 16-bit Reg */
-  return (LCD->LCD_RAM);
- }
-
-
-static void LCD_WriteReg(uint8_t LCD_Reg, uint16_t LCD_RegValue)
- {
-
-  /* Write 16-bit Index, then Write Reg */
-  LCD->LCD_REG = LCD_Reg;
-
-  /* Write 16-bit Reg */
-  LCD->LCD_RAM = LCD_RegValue;
- }
 
 
