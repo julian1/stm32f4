31fc5e147e77df44011df09d10114c45d2e866ee
julian1 <mail@julian1.io>
2021-12-20 16:10:23 +1100

    update

--- projects/control-panel-2/src/agg_test2.cpp
+++ projects/control-panel-2/src/agg_test2.cpp
@@ -42,9 +42,9 @@
     EXTR. the spi send.   does not need to wait for the send. just put in a buffer - and use interupts to process the next byte.
       so there is no waiting on the spi output.  good.
     - like using a queue to communicate info between two separate threads.
-    
 
-  - use coroutine. on the draw. and/or perhaps queue of drawing operations. 
+
+  - use coroutine. on the draw. and/or perhaps queue of drawing operations.
 
   screen mcu v hardware mcu.
   ------
@@ -114,123 +114,126 @@ extern int arial_glyph_advance_y[256] ;
 using namespace agg;
 
 
-    // eg. from agg_pixfmt_rgb_packed.h
+// eg. from agg_pixfmt_rgb_packed.h
 
-    //===========================================pixfmt_alpha_blend_rgb_packed
-    template<class Blender,  class RenBuf> class pixfmt_alpha_blend_rgb_packed
+//===========================================pixfmt_alpha_blend_rgb_packed
+template<class Blender,  class RenBuf> class pixfmt_alpha_blend_rgb_packed
+{
+public:
+
+    // for consumers
+    typedef RenBuf   rbuf_type;
+    typedef typename rbuf_type::row_data row_data;
+    typedef Blender  blender_type;
+    typedef typename blender_type::color_type color_type;
+/*
+    typedef typename blender_type::pixel_type pixel_type;
+    typedef int                               order_type; // A fake one
+    typedef typename color_type::value_type   value_type;
+    typedef typename color_type::calc_type    calc_type;
+    enum base_scale_e
     {
-    public:
-        typedef RenBuf   rbuf_type;
-        typedef typename rbuf_type::row_data row_data;
-        typedef Blender  blender_type;
-        typedef typename blender_type::color_type color_type;
-        typedef typename blender_type::pixel_type pixel_type;
-        typedef int                               order_type; // A fake one
-        typedef typename color_type::value_type   value_type;
-        typedef typename color_type::calc_type    calc_type;
-        enum base_scale_e
-        {
-            base_shift = color_type::base_shift,
-            base_scale = color_type::base_scale,
-            base_mask  = color_type::base_mask,
-            pix_width  = sizeof(pixel_type),
-        };
+        base_shift = color_type::base_shift,
+        base_scale = color_type::base_scale,
+        base_mask  = color_type::base_mask,
+        pix_width  = sizeof(pixel_type),
+    };
 
-
+*/
 
 
 public:
+  /*
+    EXTR. IMPORTANT
+    - So. we could actually do a memory read of the pixel data. for the line.
+    - to do proper blending.
+    - in order that we do not have to pass an additional fake background color.
+    - this is in fact a real advantage of using a bitmap rather than blt fill operations.
+    - most stuff is sold fill though - so reading shouldn't be too costly.
+    ------
+    alternatively we could do a gamma threshold of the coverage/ for non aa.
+  */
+
+    /*
+      EXTR.
+        need to fill a rect. with agg.
+        it may be better to affine rotate the character... maybe on load.
+        see rb.copy_bar()
+
+      - need to handle vertical reversed . how is this done normally with agg. with the render buffer?
+      - not sure. if we used bottom left. then everything would be nice.
+      arduino gfx.
+        The coordinate system places the origin (0,0) at the top left corner, with positive X increasing to the right and positive Y increasing downward.
+      EXTR.
+        i think that always rendering is equivalent to gamma==1.
+      EXTR.
+        need to invert the char geometry.
+
+    */
+
+    explicit pixfmt_alpha_blend_rgb_packed(/* rbuf_type& rb */) //:
+        // m_rbuf(&rb)
+    {}
+
+
+    AGG_INLINE unsigned width()  const { return 480;  }
+    AGG_INLINE unsigned height() const { return 272; }
+    // AGG_INLINE int      stride() const { return m_rbuf->stride(); }
+
+    AGG_INLINE void copy_hline(int x, int y,
+                               unsigned len,
+                               const color_type& c)
+    {
+    /*
+      // std::cout << "copy_hline       x " << x << " y " << y << " len " << len << " (r " << int(c.r) << " g " << int(c.g) << " b " << int(c.b) << ")"  << std::endl;
+    */
+      setXY(x, y, x + len, y + 1);   // y + 1 ????
+      for( int i = 0; i < len; ++i ) {
+        LCD_WriteData(   packRGB565( c.r, c.g, c.b)  ) ;
+      }
+
+    }
+
+
+
+    void blend_solid_hspan(int x, int y,
+                           unsigned len,
+                           const color_type& c,
+                           const int8u* covers)
+    {
       /*
-        EXTR. IMPORTANT
-        - So. we could actually do a memory read of the pixel data. for the line.
-        - to do proper blending.
-        - in order that we do not have to pass an additional fake background color.
-        - this is in fact a real advantage of using a bitmap rather than blt fill operations.
-        - most stuff is sold fill though - so reading shouldn't be too costly.
-        ------
-        alternatively we could do a gamma threshold of the coverage/ for non aa.
+      std::cout << "blend_solid_hspan x " << x << " y " << y << " len " << len << " (r " << int(c.r) << " g " << int(c.g) << " b " << int(c.b) << ")"  ;
+      std::cout << " covers ";
+      for(unsigned i = 0; i < len; ++i ) {
+        std::cout << int(covers[i]) << ", ";
+      }
+      std::cout << std::endl;
       */
 
-        /*
-          EXTR.
-            need to fill a rect. with agg.
-            it may be better to affine rotate the character... maybe on load.
-            see rb.copy_bar()
-
-          - need to handle vertical reversed . how is this done normally with agg. with the render buffer?
-          - not sure. if we used bottom left. then everything would be nice.
-          arduino gfx.
-            The coordinate system places the origin (0,0) at the top left corner, with positive X increasing to the right and positive Y increasing downward.
-          EXTR.
-            i think that always rendering is equivalent to gamma==1.
-          EXTR.
-            need to invert the char geometry.
-
-        */
-
-        explicit pixfmt_alpha_blend_rgb_packed(/* rbuf_type& rb */) //:
-            // m_rbuf(&rb)
-        {}
+      setXY(x, y, x + len, y + 1);   // how is this working without y + 1 ????
+      for( int i = 0; i < len; ++i ) {
+        LCD_WriteData(   packRGB565( c.r, c.g, c.b)  ) ;
+      }
 
 
-        AGG_INLINE unsigned width()  const { return 480;  }
-        AGG_INLINE unsigned height() const { return 272; }
-        // AGG_INLINE int      stride() const { return m_rbuf->stride(); }
+    }
 
-        AGG_INLINE void copy_hline(int x, int y,
-                                   unsigned len,
-                                   const color_type& c)
-        {
-        /*
-          // std::cout << "copy_hline       x " << x << " y " << y << " len " << len << " (r " << int(c.r) << " g " << int(c.g) << " b " << int(c.b) << ")"  << std::endl;
-        */
-          setXY(x, y, x + len, y + 1);   // y + 1 ????
-          for( int i = 0; i < len; ++i ) {
-            LCD_WriteData(   packRGB565( c.r, c.g, c.b)  ) ;
-          }
+    void blend_hline(int x, int y,
+                     unsigned len,
+                     const color_type& c,
+                     int8u cover)
+    {
+    /*
+      std::cout << "blend_hline       x " << x << " y " << y << " len " << len << " (r " << int(c.r) << " g " << int(c.g) << " b " << int(c.b) << ")"  << " cover " << int(cover) << std::endl;
+    */
+      setXY(x, y, x + len, y + 1);   // how is this working without y + 1 ????
+      for( int i = 0; i < len; ++i ) {
+        LCD_WriteData(   packRGB565( c.r, c.g, c.b)  ) ;
+      }
 
-        }
+    }
 
-
-
-        void blend_solid_hspan(int x, int y,
-                               unsigned len,
-                               const color_type& c,
-                               const int8u* covers)
-        {
-          /*
-          std::cout << "blend_solid_hspan x " << x << " y " << y << " len " << len << " (r " << int(c.r) << " g " << int(c.g) << " b " << int(c.b) << ")"  ;
-          std::cout << " covers ";
-          for(unsigned i = 0; i < len; ++i ) {
-            std::cout << int(covers[i]) << ", ";
-          }
-          std::cout << std::endl;
-          */
-
-          setXY(x, y, x + len, y + 1);   // how is this working without y + 1 ????
-          for( int i = 0; i < len; ++i ) {
-            LCD_WriteData(   packRGB565( c.r, c.g, c.b)  ) ;
-          }
-
-
-        }
-
-        void blend_hline(int x, int y,
-                         unsigned len,
-                         const color_type& c,
-                         int8u cover)
-        {
-        /*
-          std::cout << "blend_hline       x " << x << " y " << y << " len " << len << " (r " << int(c.r) << " g " << int(c.g) << " b " << int(c.b) << ")"  << " cover " << int(cover) << std::endl;
-        */
-          setXY(x, y, x + len, y + 1);   // how is this working without y + 1 ????
-          for( int i = 0; i < len; ++i ) {
-            LCD_WriteData(   packRGB565( c.r, c.g, c.b)  ) ;
-          }
-
-        }
-
-    };
+};
 
 
 
@@ -285,11 +288,74 @@ public:
         bit complicated because depends on mtx.
 */
 
+
+
+/*
+  What should we pass around????? the pixfmt???
+  perhaps yes. so if we ever want to store state on it we can.
+*/
+
+
+
+// packed rgb565
+typedef ::pixfmt_alpha_blend_rgb_packed<agg::blender_rgb565, agg::rendering_buffer> pixfmt_t;
+
+
+/*
+  so we would move this into an include file? ....
+  and then pass it from main?
+*/
+
+// template< class T>
+void drawText(pixfmt_t &pixf, agg::trans_affine &mtx, const char *s)
+{
+  // not even sure if it makes sense to factor this
+
+
+    agg::renderer_base<pixfmt_t>   rb(pixf);
+
+    // we can move these out of loop if desired...
+    agg::rasterizer_scanline_aa<> ras;
+    agg::scanline_p8 sl;
+
+
+    int x  = 0;
+
+   uint32_t start = system_millis;
+
+    for( const char *p = s; *p; ++p)  {
+
+        char ch = *p;
+
+        // whoot it links
+        font_path_type *path = arial_glyph[ ch ];
+        assert( path );
+
+        // TODO add a translate() method... or add an adapter
+        // should add a method translate( dx, dy );
+        path->m_dx = x;
+
+        x += arial_glyph_advance_x[ ch ];
+
+
+        agg::conv_transform<font_path_type> trans( *path, mtx);
+        agg::conv_curve<agg::conv_transform<  font_path_type > > curve(trans);
+
+        ras.reset();
+        ras.add_path( curve );
+        agg::render_scanlines_aa_solid(ras, sl, rb, agg::rgba(0,0,1));
+
+    }
+
+
+    usart_printf("agg text draw %ums\n", system_millis - start );
+
+
+}
+
+
 int agg_test2()
 {
-
-    // packed rgb565
-    typedef ::pixfmt_alpha_blend_rgb_packed<agg::blender_rgb565, agg::rendering_buffer> pixfmt_t;
 
 
     pixfmt_t  pixf;
@@ -323,43 +389,9 @@ int agg_test2()
     mtx *= agg::trans_affine_rotation(10.0 * 3.1415926 / 180.0);
     mtx *= agg::trans_affine_scaling(2.0); // now scale it
 
-    // we can move these out of loop if desired...
-    agg::rasterizer_scanline_aa<> ras;
-    agg::scanline_p8 sl;
-
 
     const char *s = "hello123";
-    int x  = 0;
-
-    start = system_millis;
-
-    for( const char *p = s; *p; ++p)  {
-
-        char ch = *p;
-
-        // whoot it links
-        font_path_type *path = arial_glyph[ ch ];
-        assert( path );
-
-        // TODO add a translate() method... or add an adapter
-        // should add a method translate( dx, dy );
-        path->m_dx = x;
-
-        x += arial_glyph_advance_x[ ch ];
-
-
-        agg::conv_transform<font_path_type> trans( *path, mtx);
-        agg::conv_curve<agg::conv_transform<  font_path_type > > curve(trans);
-
-        ras.reset();
-        ras.add_path( curve );
-        agg::render_scanlines_aa_solid(ras, sl, rb, agg::rgba(0,0,1));
-
-    }
-
-
-    usart_printf("agg text draw %ums\n", system_millis - start );
-
+    drawText(pixf, mtx, s );
 
 
     // non anti aliased.
