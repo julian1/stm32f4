73d81b79633219381af224563c4e29f7d3609366
julian1 <git@julian1.io>
2021-12-29 09:19:50 +1100

    update

--- projects/control-panel-2/include/fonts.h
+++ projects/control-panel-2/include/fonts.h
@@ -1,27 +1,24 @@
 
 #include <stdint.h>
 
-struct FontSpans
-{
-  uint8_t *glyph_spans[256];
-
-  int glyph_advance_x[256];
-
-  int glphy_advance_y[256];
-};
-
-
 #include "agg_path_storage_integer.h"
 
 typedef agg::serialized_integer_path_adaptor<short int, 6>  path_type;
 
 
+struct FontSpans
+{
+  uint8_t *glyph_spans[256];
+  int glyph_advance_x[256];
+  int glphy_advance_y[256];
+};
+
+
+
 struct FontOutline
 {
   path_type *glyph[256];
-
   int glyph_advance_x[256];
-
   int glphy_advance_y[256];
 };
 
@@ -29,5 +26,6 @@ struct FontOutline
 
 
 
+extern FontSpans arial_span_1p8 ;
 
-
+extern FontOutline arial_outline;
