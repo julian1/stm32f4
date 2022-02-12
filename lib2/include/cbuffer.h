d9900f1b5ad1033755db0857d7664e8e6ddadfb9
julian1 <git@julian1.io>
2022-02-12 12:05:49 +1100

    remove old streams code from cbuffer

--- lib2/include/cbuffer.h
+++ lib2/include/cbuffer.h
@@ -50,32 +50,13 @@ int32_t cBufCopyString(CBuf *a, char *p, size_t n); // change name readString...
 int32_t cBufCopyString2(const CBuf *a, char *p, size_t n);
 
 
-#if 0
-// don't think these belong here, as primitives
-//
-int32_t cBufMark(CBuf *a);
-void cBufReverse(CBuf *a, int count);
-void indentLeft(CBuf *a, int count);
-#endif
-
-// can implement separate, non-string copy funcs, without sentinel for byte array /mem application functions
-// non terminated...
-
 ////////////////
 int32_t cBufRead(CBuf *a, char *p, size_t n);
 ssize_t cBufWrite(CBuf *x, const char *buf, size_t size);
 
+// can implement separate, non-string copy funcs, without sentinel for byte array /mem application functions
+// non terminated...
 
-FILE * cBufMakeStream( CBuf *x );   // change name cBufMkStream ? to be able to write...
-
-////////////
-
-void cBufprintf( CBuf *cookie,
-  ssize_t (*cBufWrite)(CBuf *x, const char *buf, size_t size),
-  const char *format, ...);
-
-
-void cBufWriteStream(CBuf *x, FILE *stream);
 
 
 
