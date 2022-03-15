3c55d657054ca14797c8a22c2f7d7a9c45917294
julian1 <mail@julian1.io>
2022-03-16 09:50:57 +1100

    factor flash/stream functions from matrix serialization

--- projects/slope-adc-stm32f411ceu/include/flash.h
+++ projects/slope-adc-stm32f411ceu/include/flash.h
@@ -3,7 +3,6 @@
 
 #include <stdio.h> // FILE
 
-#include "matrix.h"
 
 // bad conflicts with lib2/include/flash.h
 
@@ -11,14 +10,6 @@ void flash_read(void);
 void flash_write(void);
 
 void flash_erase_sector1(void);
-
-// void m_write_flash ( MAT *m );
-void m_write_flash ( MAT *m , FILE *f);
-
-// MAT * m_read_flash( MAT *out);
-MAT * m_read_flash( MAT *out, FILE *f);
-
-
 
 FILE * open_flash_file(void);
 // long ftell2( FILE *f);
