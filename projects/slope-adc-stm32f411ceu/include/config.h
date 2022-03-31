ac5f1570071090a2b77364f2847d8f1a08c0fc00
julian1 <mail@julian1.io>
2022-03-31 11:57:08 +1100

    comment
    comment

--- projects/slope-adc-stm32f411ceu/include/config.h
+++ projects/slope-adc-stm32f411ceu/include/config.h
@@ -5,23 +5,27 @@
 #include "matrix.h"
 
 
+// rename file_skip_to_end
+
 void c_skip_to_end(  FILE *f);
 #if 0
 int c_skip_to_last_valid(  FILE *f);
 #endif
 
 
-// int c_scan(  FILE *f);
-// int c_scan( FILE *f, MAT *b, unsigned b_sz );
+// rename file_scan_values.
+
 int c_scan( FILE *f, MAT **b, unsigned b_sz );
 
 
+/*
+  these are poorly named.
+  they write a header, specific for iterating through nor flash.
+  but are using the generic stream interface
+*/
 
-// void m_write_flash ( MAT *m );
-// void m_write_flash ( MAT *m , FILE *f);
 void m_write_flash ( MAT *m , int slot, FILE *f);
 
-// MAT * m_read_flash( MAT *out);
 MAT * m_read_flash( MAT *out, FILE *f);
 
 
