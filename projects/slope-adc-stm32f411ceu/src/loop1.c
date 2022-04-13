6775d25dcd2bf9570adfab5633570573269e25c2
julian1 <mail@julian1.io>
2022-04-13 12:12:13 +1000

    comment
    comment

--- projects/slope-adc-stm32f411ceu/src/loop1.c
+++ projects/slope-adc-stm32f411ceu/src/loop1.c
@@ -806,10 +806,20 @@ void app_loop4 ( app_t *app   )
 
   unsigned row = 0;
 
+
   // 22V range.
   double target_[] = {
     11, 10.5, 10, 9.5, 9, 8.5, 8, 7.5, 7, 6.5, 6, 5.5, 5, 4.5, 4, 3.5, 3, 2.5, 2, 1.5, 1, 0.5, 0,
     -0.5, -1, -1.5, -2, -2.5, -3, -3.5, -4, -4.5, -5, -5.5, -6, -6.5, -7, -7.5, -8, -8.5, -9, -9.5, -10, -10.5, -11 } ;
+
+
+/*
+  larger voltage jump means more DA. so this could be worse. don't use.
+  double target_[] = {
+    11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0,
+    -1, -2, -3, -4, -5, -6, -7, -8, -9, -10, -11 } ;
+*/
+
 
   // more obs == more spread due to DA.
   unsigned obs_n = 50;
@@ -977,6 +987,7 @@ void app_loop4 ( app_t *app   )
 }
 
 
+#if 0
 static void app_loop5 ( app_t *app   )
 {
   /*
@@ -989,6 +1000,7 @@ static void app_loop5 ( app_t *app   )
 
   */
 }
+#endif
 
 
 
