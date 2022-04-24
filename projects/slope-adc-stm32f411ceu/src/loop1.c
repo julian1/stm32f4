2374cde6e97da70044d788d19f682e5c55b0ed82
julian1 <git@julian1.io>
2022-04-24 20:37:49 +1000

    add nplc 16 to loop2 again

--- projects/slope-adc-stm32f411ceu/src/loop1.c
+++ projects/slope-adc-stm32f411ceu/src/loop1.c
@@ -598,12 +598,13 @@ void app_loop2 ( app_t *app )
 
   // unsigned nplc_[] = { 9, 10, 11, 12 };
   // unsigned nplc[] = { 8, 9, 10, 11, 12, 13  };
-  // unsigned nplc[] = { 8, 9, 10, 11, 12, 13, 14, 15, 16  };
-  unsigned nplc[] = { 8, 9, 10, 11, 12, 13, 14, 15 };
+  unsigned nplc[] = { 8, 9, 10, 11, 12, 13, 14, 15, 16  };
+  // unsigned nplc[] = { 8, 9, 10, 11, 12, 13, 14, 15 };
   // double y  = 0;
 
   unsigned obs_n = 7; // 7
   // unsigned obs_n = 25; // 7
+  // unsigned obs_n = 10;
 
   /*
   just use the input parameter...
