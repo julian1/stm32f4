c8eabc4c82a6d2179cd701e975d8cfb55014655c
julian1 <mail@julian1.io>
2023-10-06 10:56:01 +1100

    test15 switch azmux also

--- projects/dmm/src/main.c
+++ projects/dmm/src/main.c
@@ -799,7 +799,7 @@ static void update_console_cmd(app_t *app)
         spi_ice40_reg_write32(app->spi, REG_MODE, MODE_AZ );  // mode 3. test pattern on sig
 
         //////////////
-        // use direct register - for the lo sample, in azmode.
+        // use DIRECT0 to set lo sample, in azmode.
         F  f;
         memset(&f, 0, sizeof(f));
         f.himux2 = S1 ;       // s1 put dc-source on himux2 output
@@ -809,7 +809,7 @@ static void update_console_cmd(app_t *app)
         // f.azmux  = SOFF ;     // soff for high-z for test.
         spi_ice40_reg_write_n(app->spi, REG_DIRECT, &f, sizeof(f) );
 
-        // use direct 2 register - for the hi sample
+        // use DIRECT 2 to set the hi sample
         memset(&f, 0, sizeof(f));
         f.azmux  = S1 ;         // s1 == PC_OUT (either SIG or BOOT).
         // f.azmux  = S2 ;      // s2 == BOOT for test.
@@ -837,9 +837,10 @@ static void update_console_cmd(app_t *app)
 
         /*
         // test charge-injection by charging to a bias voltage, holding, then entering az mode.
-        // with az-mux off through cycle - in order not eliminate amplifier load leakage .
+        // with az-mux held off through cycle - which eliminates effect of leakage of amplifer (if fitted) input jfets.
         // first argument - bias voltage.  10,-10,0
         // second argument is nplc.
+        // note t
 
         */
         printf("test leakage and charge-injection from switching pre-charge switch at different biases\n");
@@ -969,72 +970,175 @@ static void update_console_cmd(app_t *app)
         /*
 
 
-        Briefly for sn74lv4053a one difference with the previous tests - is the zener to set the boot supply rail.
-        but tests show that a bootstrap supply rail between 4V to 5.5V doesn'matter much for leakage or charge injection.
-        Also tried another sn74lv4053a, purchased a few years apart from the initial test one, but with the same result.
-        So i am not sure how to explain the discrepency with previous tests.
+          Still exploring cmos switches for a bit,
+
+          Briefly for sn74lv4053a one difference with the previous tests - is the zener used to set the boot supply rail.
+          but tests show that a bootstrap supply rail between 4V to 5.5V doesn't matter much for leakage or charge injection.
+          Also tried another sn74lv4053a, purchased a few years apart from the one used for initial tests, but with the same result.
+          So i am not sure how to explain the discrepancy with previous test results.
+
+          Running az modulation. all muxes fitted.
+          DC accumulation on 10nF/ over 10s.
+
+          test14.
+          sn74lv4053a
+          +10V dc bais
+          1000nplc/off   20mV. 18mV.
+          100nplc/2s     17mV. 17mV.
+          10nplc/200ms   21mV. 22mV.
+          1nplc/20ms     35mV. 73mV.  70mV.   large measured difference. odd. but was definltey there.
 
 
-        Running az modulation. all muxes fitted.
-        DC accumulation on 10nF/ over 10s.
+          But max4053 looks a lot better,
+          I almost wasn't going to bother re-testing it, based on past resulsts.
+          Identical setup as above - accumulation on 10nF/ 10s.
 
-        sn74lv4053a
-        +10V dc bais
-        1000nplc/off   20mV. 18mV.
-        100nplc/2s     17mV. 17mV.
-        10nplc/200ms   21mV. 22mV.
-        1nplc/20ms     35mV. 73mV.  70mV.   large measured difference. odd. but was definltey there.
+          max4053
+          +10V dc bias.
+          1000nplc/off   0.3mV. 0.5mV
+          100nplc        0.8mV.
+          10nplc         3.8mV.   3.6mV.
+          1nplc          30mV.   28mV.
 
+          max4053
+          -10V dc bias.
+          leave five minutes for +4.5mV/10s. cap DA to settle.
+          1000nplc/off   2.5mV  2.8mV   - oct 8  2.3mV.
+          100nplc        3.0mV. 3.3mV   - oct 8.  2.3mV
+          10nplc         5.6mV.  5.7mV  - oct 8. 5.2mV.
+          1nplc          30mV   30mV.   - oct 8  29mV.
 
-        But max4053 looks a lot better,
-        I almost wasn't going to bother re-testing.
-        same conditions - accumulation on 10nF/ 10s.
+          max4053
+          0V dc bias.
+          1000nplc/off   0.8mV.
+          100nplc        1.0mV.  1mV.
+          10nplc         3.8mV.  3.6mV.
+          1nplc          28mV.
 
-        max4053
-        +10V dc bias.
-        1000nplc/off   0.3mV. 0.5mV
-        100nplc        0.8mV.
-        10nplc         3.8mV.   3.6mV.
-        1nplc          30mV.   28mV.
+          leakage is more controlled -  <1pA for +10V and 0V, and <3pA for -10V dc-bias.
 
+          for charge injection
+          ie. 1nplc == 20ms.  10s/0.02s == 500 cycles.
+          this is 30mV / 500 == 0.06pC .
+          if I have the units correct, through full-cycle switch.
 
-        max4053
-        -10V dc bias.
-        leave five minutes for +4.5mV/10s. cap DA to settle.
-        1000nplc/off   2.5mV  2.8mV   - oct 8  2.3mV.
-        100nplc        3.0mV. 3.3mV   - oct 8.  2.3mV
-        10nplc         5.6mV.  5.7mV  - oct 8. 5.2mV.
-        1nplc          30mV   30mV.   - oct 8  29mV.
-
-        max4053
-        0V dc bias.
-        1000nplc/off   0.8mV.
-        100nplc        1.0mV.  1mV.
-        10nplc         3.8mV.  3.6mV.
-        1nplc          28mV.
+          The above tests were done with the azmux held off, with only the pre-charge switch switching.
+          this would eliminate/isolate any leakage through the amplifer input jfets (if fitted) .
 
 
-        leakage is fairly good <1pA for +10V and 0V dc-bias, and <3pA .
+          test15.
+          When the azmux also changed to for normal sampling beetween PC-OUT (S1) and LO (S6), the result is similar - eg good.
 
-        for charge injection
-        ie. 1nplc == 20ms.  10s/0.02s == 500 cycles.
-        this is 30mV / 500 == 0.06pC .
-        if I have the units correct, through full-cycle switch.
+          max4053
+          0V dc bias.
+          1000nplc/off   1.3mV 1.2mV.
+          100nplc        1.8mV
+          10nplc         4.8mV
+          1nplc          38mV. 37mV.
 
-
-
-
-        for the leakage part - it is a little higher with a negative bias. and possibly the adg1208 mux, not the  4053. still looks ok.
-        -------
-
-        since there is nothing on the output of the azmux - should be able to switch lo,hi also. and observe.
         */
+      }
+
+
+    else if( sscanf(cmd, "test15 %ld %lu", &i0, &u1 ) == 2) {
+
+        /*
+        // test charge-injection by charging to a bias voltage, holding, then entering az mode.
+        // with az-mux also switching .
+        // first argument - bias voltage.  10,-10,0
+        // second argument is nplc.
+        // note t
+
+        */
+        printf("test leakage and charge-injection from switching pre-charge switch at different biases\n");
+        app->test_in_progress = 0;
+        Mode j = mode_initial;
+
+        if(i0 == 10) {
+          printf("with +10V\n");
+          j.second.U1003  = S1 ;       // s1. dcv-source s1. +10V.
+          j.second.U1006  = S1 ;       // s1.   follow  .   dcv-mux2
+        }
+        else if(i0 == -10) {
+          printf("with -10V\n");
+          j.second.U1003  = S2 ;       // s2.  -10V.
+          j.second.U1006  = S1 ;       // s1.   follow  .   dcv-mux2
+        }
+        else if(i0 == 0) {
+          printf("with 0V\n");
+          j.second.U1003 = S3;          // s3 == agnd
+          j.second.U1006 = S6;          // s6 = agnd  .  TODO change to S7 . populate R1001.c0ww
+        }
+        else assert(0);
+
+        // turn on accumulation relay     RON ROFF.  or RL1 ?  K606_ON
+        j.first .K406_CTL  = 0b01;
+        j.second.K406_CTL  = 0b00;    // don't need this....  it is 0 by default
+
+        do_4094_transition( app->spi, &j,  &app->system_millis );
+
+        /////////////////
+        // make sure we are in direct mode.
+        mux_ice40(app->spi);
+        spi_ice40_reg_write32(app->spi, REG_MODE, MODE_DIRECT );
+
+        // now control the hi mux.
+        F  f;
+        memset(&f, 0, sizeof(f));
+        f.himux2 = S1 ;    // s1 put dc-source on himux2 output
+        f.himux  = S2 ;    // s2 reflect himux2 on himux output
+        f.sig_pc_sw_ctl  = 1;  // turn on. precharge.  on. to route signal to az mux... doesn't matter.
+        spi_ice40_reg_write_n(app->spi, REG_DIRECT, &f, sizeof(f) );
+
+        ////////////////////////////
+        // so charge cap to the dcv-source
+        // and let settle 10sec. for DA....
+        printf("sleep 10s\n");  // having a yield would be quite nice here.
+        msleep(10 * 1000,  &app->system_millis);
+
+        /////////////////
+        // now change to az mode.
+        printf("changing to az mode.\n");  // having a yield would be quite nice here.
+        // setup az mode
+        mux_ice40(app->spi);
+        spi_ice40_reg_write32(app->spi, REG_MODE, MODE_AZ );  // mode 3. test pattern on sig
+
+        //////////////
+        // use direct register - for the lo sample, in azmode.
+
+
+
+        memset(&f, 0, sizeof(f));
+        f.himux2 = SOFF ;
+        f.himux  = SOFF ;
+        f.azmux  = S6 ;    // s6 == normal LO for DCV, ohms.
+        spi_ice40_reg_write_n(app->spi, REG_DIRECT, &f, sizeof(f) );
+
+        // set the hi for az.
+        f.azmux  = S1 ;         // s1 == PC_OUT (either SIG or BOOT).
+        spi_ice40_reg_write_n(app->spi, REG_DIRECT2, &f, sizeof(f) );
+
+
+        assert(u1 == 1 || u1 == 10 || u1 == 100 || u1 == 1000); // not really necessary. just avoid mistakes
+
+         uint32_t aperture = nplc_to_aper_n( u1 );
+
+        printf("aperture %lu\n",   aperture );
+        printf("nplc     %.2lf\n",  aper_n_to_nplc( aperture ));
+        printf("period   %.2lfs\n", aper_n_to_period( aperture ));
+
+        spi_ice40_reg_write32(app->spi, REG_CLK_SAMPLE_DURATION, aperture );
+
+
+        // 0V dc bias.
+        // 1000nplc/off   1.3mV 1.2mV.
+        // 100nplc        1.8mV
+        // 10nplc         4.8mV
+        // 1nplc          38mV. 37mV.
 
 
 
       }
-
-
 
       else if( strcmp( cmd , "") == 0) {
 
