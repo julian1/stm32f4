d002c63587e1619475c19cb0bcadc4e1b537621e
julian1 <git@julian1.io>
2023-10-14 14:36:09 +1100

    fix constness

--- projects/dmm/include/spi-ice40.h
+++ projects/dmm/include/spi-ice40.h
@@ -13,7 +13,7 @@ uint32_t spi_ice40_reg_write32(uint32_t spi, uint8_t reg, uint32_t val);
 uint32_t spi_ice40_reg_read32(uint32_t spi, uint8_t reg);
 
 
-uint32_t spi_ice40_reg_write_n(uint32_t spi, uint8_t reg, void *s, size_t n );
+uint32_t spi_ice40_reg_write_n(uint32_t spi, uint8_t reg, const void *s, size_t n );
 
 
 
