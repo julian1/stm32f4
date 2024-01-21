bcfb2f5425d6f460a38b0416c75a827fadb1e745
julian1 <mail@julian1.io>
2024-01-22 06:18:14 +1100

    tidy

--- include/lib2/format.h
+++ include/lib2/format.h
@@ -16,15 +16,15 @@ extern "C" {
 char * str_trim_whitespace_inplace( char *cmd );
 
 
-extern char * str_indent_left(char *s, size_t sz, int indent, const char *string);
-extern char * str_indent_right(char *s, size_t sz, int indent, const char *string);
-extern char * snprintf2(char *s, size_t sz, const char *format, ...);
+char * str_indent_left(char *s, size_t sz, int indent, const char *string);
+char * str_indent_right(char *s, size_t sz, int indent, const char *string);
+char * snprintf2(char *s, size_t sz, const char *format, ...);
 
 // change name format_double...
-extern char * str_format_float(char *s, size_t sz, int digits, double value);
-extern char * str_format_float_with_commas(char *s, size_t sz, int digits, double value);
+char * str_format_float(char *s, size_t sz, int digits, double value);
+char * str_format_float_with_commas(char *s, size_t sz, int digits, double value);
 
-extern char * str_format_bits(char *buf, size_t width, uint32_t value);
+char * str_format_bits(char *buf, size_t width, uint32_t value);
 
 
 #ifdef __cplusplus
