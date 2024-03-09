3a9daecc8d07f485e5fea2e23eb20c12dc78f236
julian1 <git@julian1.io>
2024-03-09 17:31:39 +1100

    add extra sa registers

--- projects/minimal/include/ice40-reg.h
+++ projects/minimal/include/ice40-reg.h
@@ -2,8 +2,9 @@
 #pragma once
 
 /*
-  don't move to mode.h
-  because it drags high-level unrelated app state / 4094 stuff for simple spi peripherals.
+  fpga specific
+  this doeesn't belong in mode.h
+  because then it would be pulled in with all the 4094 stuff and for spi peripherals.
 
 
 */
@@ -14,8 +15,7 @@
 
 
 #define REG_SPI_MUX       8
-#define REG_4094          9     // add suffix. or reg_4094_cr configuration-register. or OE or something.
-
+#define REG_4094          9     // rename, add suffix. or reg_4094_cr config-register. or OE or something.
 #define REG_MODE          12
 #define REG_DIRECT        14
 #define REG_STATUS        17
@@ -23,8 +23,14 @@
 
 
 //  sample acquisition.
-#define REG_SA_ARM_TRIGGER              19
+#define REG_SA_ARM_TRIGGER              19      // TODO reassign
 #define REG_SA_P_CLK_COUNT_PRECHARGE    21
+#define REG_SA_P_AZMUX_LO_VAL           22
+#define REG_SA_P_AZMUX_HI_VAL           23
+#define REG_SA_P_SW_PC_CTL_HI_VAL       24
+
+
+
 
 // adc parameters
 #define REG_ADC_P_CLK_COUNT_APERTURE    20   // clk sample time. change name aperture.  // TODO reassign.
