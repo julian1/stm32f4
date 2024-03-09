c9cfaa0811119d9a17e6c29b39878e4262531d69
julian1 <git@julian1.io>
2024-03-10 08:30:19 +1100

    reassign fpga registers

--- projects/minimal/include/ice40-reg.h
+++ projects/minimal/include/ice40-reg.h
@@ -14,27 +14,41 @@
 // SR_STATUS
 
 
-#define REG_SPI_MUX       8
-#define REG_4094          9     // rename, add suffix. or reg_4094_cr config-register. or OE or something.
-#define REG_MODE          12
-#define REG_DIRECT        14
-#define REG_STATUS        17
+#define REG_SPI_MUX                     8
+#define REG_4094                        9
+#define REG_MODE                        12
+#define REG_DIRECT                      14
+#define REG_STATUS                      17
 
 
+///////////////////////
 
 //  sample acquisition.
-#define REG_SA_ARM_TRIGGER              19      // TODO reassign
+#define REG_SA_ARM_TRIGGER              20
 #define REG_SA_P_CLK_COUNT_PRECHARGE    21
 #define REG_SA_P_AZMUX_LO_VAL           22
 #define REG_SA_P_AZMUX_HI_VAL           23
 #define REG_SA_P_SW_PC_CTL_HI_VAL       24
 
 
-
-
 // adc parameters
-#define REG_ADC_P_CLK_COUNT_APERTURE    20   // clk sample time. change name aperture.  // TODO reassign.
-#define REG_ADC_P_CLK_COUNT_RESET       25
+#define REG_ADC_P_CLK_COUNT_APERTURE    30
+#define REG_ADC_P_CLK_COUNT_RESET       31
+
+
+// adc counts
+#define REG_ADC_CLK_COUNT_REFMUX_RESET  40
+#define REG_ADC_CLK_COUNT_REFMUX_NEG    41
+#define REG_ADC_CLK_COUNT_REFMUX_POS    42
+#define REG_ADC_CLK_COUNT_REFMUX_RD     43
+#define REG_ADC_CLK_COUNT_MUX_SIG       44
+
+
+// extra stat counts.
+#define REG_ADC_STAT_COUNT_REFMUX_POS_UP  50
+#define REG_ADC_STAT_COUNT_REFMUX_NEG_UP  51
+#define REG_ADC_STAT_COUNT_CMPR_CROSS_UP  52
+
 
 
 
