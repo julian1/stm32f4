bac6786ebfc36f2a061b676c9458e4735a1b340d
julian1 <git@julian1.io>
2024-03-18 10:00:17 +1100

    revert bad change to dmm file

--- projects/dmm/src/cal.c
+++ projects/dmm/src/cal.c
@@ -240,8 +240,6 @@ void app_cal( app_t *app )
 
   printf("xs\n");
   m_foutput( stdout, xs );
-  
-  // fflush(stdout) is not equivalent. since it doesn't know about the usart1 buffer.
   usart1_flush(); // block
 
 
@@ -262,12 +260,12 @@ void app_cal( app_t *app )
   R regression;
 
   m_regression( xs, y, &regression );   // rename reg_regression()...
-  usart1_flush();   // block
+  usart1_flush();
 
   // TODO change name regression_report.
   // or _output..
   r_regression_show( &regression, stdout);
-  usart1_flush(); // block
+   usart1_flush();
 
 
 
