5c7f126d64992d9cfe49e3b1076aa9fcc788d499
julian1 <git@julian1.io>
2024-05-25 20:02:12 +1000

    swap test01, test02, tidy

--- projects/minimal/src/test/test01.c
+++ projects/minimal/src/test/test01.c
@@ -1,16 +1,71 @@
 
 /*
-  more an example of how to use embedded repl.
-  need to call sleep though. sleep .
+  blink the fpga leds in a nice pattern.
+  using direct mode.
+  shouldn't need fpga clk.
 
+  use when first populating board, to test mcu/fpga and spi comms
+  also checks the reg_direct value.
+
+  note power consupmption - mcu 21mA. fpga 19mA.  with cmos oscillator running.
+  but needs more on first power up- else slow cap charge, and won't start.
 */
 
 #include <stdio.h>
 #include <assert.h>
-#include <string.h>   // strcmp
+#include <string.h>         // strcmp
+
+
+#include <lib2/util.h>     // msleep()
+#include <lib2/format.h>   // format_bits()
+
+
 
 #include <app.h>
-// #include <lib2/util.h>         // msleep()
+#include <peripheral/spi-ice40.h>
+#include <ice40-reg.h>
+
+
+
+
+
+static void test (app_t *app)
+{
+
+  spi_mux_ice40( app->spi );
+
+  // put direct mode
+  spi_ice40_reg_write32( app->spi, REG_MODE , MODE_DIRECT );
+
+
+
+  for(unsigned i = 0; i < 50; ++i )  {
+
+    static uint32_t counter = 0;
+    ++counter;
+    uint32_t magic = counter  ^ (counter >> 1);
+    /*
+    static uint32_t magic = 0;
+    ++magic;
+    */
+
+    // blink led... want option. so can write reg_direct
+    // note - led will only, actually light if fpga in default mode. 0.
+    spi_ice40_reg_write32( app->spi, REG_DIRECT, magic );
+
+    // check the magic numger
+    uint32_t ret = spi_ice40_reg_read32( app->spi, REG_DIRECT);
+    if(ret != magic ) {
+      // comms no good
+      char buf[ 100] ;
+      printf("comms failed, returned reg value %s\n",  str_format_bits(buf, 32, ret ));
+    } else {
+      // printf("comms ok\n");
+    }
+
+    msleep( 100,  &app->system_millis);
+  }
+}
 
 
 
@@ -19,10 +74,8 @@ bool app_test01( app_t *app , const char *cmd)
   assert(app);
   assert(cmd);
 
-
   if( strcmp(cmd, "test01") == 0) {
-
-    app_repl_statements(app, "set k405 1; set k406 0; set k407 0\n" );
+    test( app );
     return 1;
   }
 
--- projects/minimal/src/test/test02.c
+++ projects/minimal/src/test/test02.c
@@ -1,66 +1,16 @@
 
 /*
-  test function, when first populating board, to test mcu/fpga spi comms
-  note this checks the return reg_direct return value. so forms a self-test.
-
-  - writes reg_direct, avoids mode. good for first test
+  more an example of how to use embedded repl.
+  need to call sleep though. sleep .
 
 */
 
 #include <stdio.h>
 #include <assert.h>
-#include <string.h>         // strcmp
-
-
-#include <lib2/util.h>     // msleep()
-#include <lib2/format.h>   // format_bits()
-
-
+#include <string.h>   // strcmp
 
 #include <app.h>
-#include <peripheral/spi-ice40.h>
-#include <ice40-reg.h>
-
-
-
-
-
-static void test (app_t *app)
-{
-
-  spi_mux_ice40( app->spi );
-
-  spi_ice40_reg_write32( app->spi, REG_MODE , MODE_DIRECT );
-
-
-
-  for(unsigned i = 0; i < 50; ++i )  {
-
-    static uint32_t counter = 0;
-    ++counter;
-    uint32_t magic = counter  ^ (counter >> 1);
-    /*
-    static uint32_t magic = 0;
-    ++magic;
-    */
-
-    // blink led... want option. so can write reg_direct
-    // note - led will only, actually light if fpga in default mode. 0.
-    spi_ice40_reg_write32( app->spi, REG_DIRECT, magic );
-
-    // check the magic numger
-    uint32_t ret = spi_ice40_reg_read32( app->spi, REG_DIRECT);
-    if(ret != magic ) {
-      // comms no good
-      char buf[ 100] ;
-      printf("comms failed, returned reg value %s\n",  str_format_bits(buf, 32, ret ));
-    } else {
-      // printf("comms ok\n");
-    }
-
-    msleep( 100,  &app->system_millis);
-  }
-}
+// #include <lib2/util.h>         // msleep()
 
 
 
@@ -72,9 +22,7 @@ bool app_test02( app_t *app , const char *cmd)
 
   if( strcmp(cmd, "test02") == 0) {
 
-    printf("here\n");
-    test( app );
-
+    app_repl_statements(app, "set k405 1; set k406 0; set k407 0\n" );
     return 1;
   }
 
