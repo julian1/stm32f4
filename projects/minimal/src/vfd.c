27a432a91e0afce8f6df153ccbd91a55964bb949
julian1 <mail@julian1.io>
2024-08-15 15:52:19 +1000

    improve vfd formatting

--- projects/minimal/src/vfd.c
+++ projects/minimal/src/vfd.c
@@ -3,6 +3,7 @@
 #include <ctype.h>        // toupper
 #include <stdio.h>
 #include <assert.h>
+#include <math.h>       // fabs
 
 
 
@@ -19,6 +20,123 @@
 
 
 #define UNUSED(x) (void)(x)
+
+
+
+
+static void string_rotate_right( char *s, size_t len,  unsigned shift )
+{
+  // rotate string right by shift digits
+  // correctly handles sentinal/terminating
+
+  if(shift == 0)
+    return;
+
+  // string_rotate_right right
+  for(unsigned i = len-1; i >= shift; --i) {
+
+    // printf("i %u   i-shift %u \n", i, i - shift );
+    assert( i < len);
+    assert( (i - shift)  < len);
+    s[ i ] = s [ i - shift ];
+  }
+
+}
+
+
+static signed strpos( char *s, size_t len, char ch)
+{
+  // like, strchr( s, ch) - s ; but no assumption string is null terminated
+  signed cur = 0;
+  for(unsigned i = 0; i < len && cur == 0 && s[i] != 0 ; ++i) {
+    if( s[i] == ch)
+      cur = i;
+  }
+  // return 0 if not found
+  return cur;
+}
+
+
+
+static void string_align_dot( char *s, size_t len, signed pos )
+{
+  /*
+    simpler way to do this - would be to calc prefix digits represented using log10().
+    eg. prefix_digits = floor(log10( aval)) + 1;
+    but doesn't quite work. because sprintf will add a single leading zero.
+    so do it using string manipulation/interrogation instead
+  */
+
+  signed cur = strpos(s, len, '.' );
+  assert(cur != 0);
+
+  // determine chars to shift
+  signed shift = pos - cur;
+  assert( shift >= 0);
+  string_rotate_right( s, len,  shift );
+
+
+  // prepend zeros
+  for(unsigned i = 0; i < (unsigned) shift; ++i) {
+    assert( i < len);
+    s[i] = '0';
+  }
+}
+
+
+
+static void format_value( char *s, size_t n, double value, unsigned leading, unsigned trailing )
+{
+
+  // snprintf(s, n, "%.6f", fabs( value ) );
+  snprintf(s, n, "%.*f", trailing, fabs(value));
+
+  string_align_dot( s, n, leading);   // eg. -12.xxx
+
+  s[ 0 ] = value >= 0 ? '+' : '-';
+}
+
+
+
+static char * intersperse_commas(char *dst, size_t sz,  const char *src)
+{
+  /*
+    sz is the dst size buffer.
+  */
+  char *d = dst;
+  const char *s = src;
+
+  bool      gotdot = false;
+  unsigned  dotdigits = 0;
+
+  while(*s && d < dst + sz) {
+
+    if( *s == '.')
+      gotdot = true;
+
+    if(gotdot && isdigit( (int) *s))
+      ++dotdigits;
+
+
+    *d++ = *s++;
+
+    // eg. comma every third digit
+    if( dotdigits != 0
+      && (dotdigits % 3) == 0
+      && d < dst + sz)
+      *d++ = ',';
+  }
+
+  // always add a terminal
+  if( d < dst + sz)
+    *d = 0;
+  else
+    *(dst + sz - 1)  = 0;
+
+
+  return dst;
+}
+
 
 
 
@@ -43,7 +161,15 @@ void vfd_update_new_reading(data_t *data)
 
   char buf[100];
 
+  // from util.  should deprecate
   str_format_float_with_commas(buf, 100, 7, data->computed_val);
+
+
+  // format_value( buf, 100 - 1, data->computed_val, 3, 6 );
+
+  // write value
+  vfd_write_bitmap_string2( buf, 0 , 0 );
+
 
 /*
   if(sample_seq_mode == SEQ_MODE_RATIO)
@@ -54,35 +180,32 @@ void vfd_update_new_reading(data_t *data)
 
 
 
-  uint8_t sample_idx      =  ADC_STATUS_SAMPLE_IDX( data->adc_status) ;       
+  uint8_t sample_idx      =  ADC_STATUS_SAMPLE_IDX( data->adc_status) ;
 
-  uint8_t sample_seq_mode =  ADC_STATUS_SAMPLE_SEQ_MODE( data->adc_status);  
+  uint8_t sample_seq_mode =  ADC_STATUS_SAMPLE_SEQ_MODE( data->adc_status);
 
 
 
-  // write value
-  vfd_write_bitmap_string2( buf, 0 , 0 );
+  // write a star, for the sample
+  vfd_write_string2( sample_idx % 2 == 0 ? "*" : " ", 0, 3 );
+
 
 
   // write mode
   seq_mode_str( sample_seq_mode, buf, 8 );
   stoupper( buf);
-  vfd_write_string2( buf, 0, 3 );
+  vfd_write_string2( buf, 0, 4 );
 
 
 
   // write nplc
   double nplc = aper_n_to_nplc( data->adc_clk_count_mux_sig, data->line_freq );
   snprintf(buf, 100, "nplc %.1lf ", nplc );
-  vfd_write_string2( buf, 0, 4 );
-
-
-  // write a star, for the sample
-  vfd_write_string2( sample_idx % 2 == 0 ? "*" : " ", 0, 5 );
+  vfd_write_string2( buf, 0, 5 );
 
 
   // write/ dummy other stuff.
-  snprintf(buf, 100, "local, range DCV, 10M" );   
+  snprintf(buf, 100, "DCV 10M" );
   vfd_write_string2( buf, 0, 6 );
 
 
