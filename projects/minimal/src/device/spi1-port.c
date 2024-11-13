dded9120e9bb0d5623de26b94f0d5a03de25e076
julian1 <mail@julian1.io>
2024-11-13 15:45:56 +1100

    cleanup

--- projects/minimal/src/device/spi1-port.c
+++ projects/minimal/src/device/spi1-port.c
@@ -1,89 +1,27 @@
 /*
-   consider file rename spi1-port.c
-    because it is specific.
 
-  ---
-  - this code is just about generic enough. to move to lib2
-  - perhaps should make generic for any spi.  and/or  prefix filename spi1-port.c  etc.
-
-  ---
-  anything through a 6 pin adum/cap isolator
-
-  Code is shared for smu and adc
-  move to shared library?
-
-  -----------
-
-  EXTR. dec 18 2022.
+  dec 18 2022.
     at 100MHz (instead of 50MHz) there are flip issues, under stress test. with iso7762
 
-  could use an and gate ic with gpio. but dificulty of synchronizing signals
 
 */
 
 #include <libopencm3/stm32/gpio.h>
-#include <libopencm3/stm32/spi.h>       // TODO REMOVE.
-
-
+// #include <libopencm3/stm32/spi.h>       // TODO REMOVE.
 #include <libopencm3/cm3/nvic.h>
 #include <libopencm3/stm32/exti.h>
 
 
 #include <stdio.h>
-#include <string.h>   // memset
-#include <stddef.h>   // NULL
 #include <assert.h>
 
 #include <device/spi-port.h>
 
-#include <support.h>  // spi wait functions
-
-
-// #include <gpio.h>
-
-//////////////////////////
-
-// #define UNUSED(x) (void)(x)
-
-
-// TODO - no reason to have the ICE40  prefix here.
-// should just name to SPI1c
-
-/*
-  jan 2024.
-  - if put all these in a data structure - then the enable/disable could be made more generic. for spi1,or spi2 etc.
-  - we would'nt have to carry around the spi integer. in app.
-  - not sure the callback works.
-  ----
-  - rather than having to prefix with spi1... everywhere.
-
-*/
-
-// For dsn257.
 
 #define SPI1_PORT       GPIOA
 #define SPI1_CS1        GPIO4     // PA4
 #define SPI1_CS2        GPIO15     // gerber157.
 #define SPI1_INTERUPT   GPIO3     // PA3
-
-// #define SPI1_CS2       GPIO10    // PA10 july 2024. nss moved.
-
-// clk,PA5   mosi, PB5.  miso PA6
-
-
-/*
-  feb 2024. splitting spi configuration over two ports (mosi on PB5) . is messy. but needed to free PA7 pin for ethernet.
-  keep the cs1 and cs2 gpio config separate
-
-
-  see hal.h  and PIN() macro for cleaner example looping an array.
-    vim ~/devel/mongoose/examples/stm32/nucleo-f429zi-make-baremetal-builtin/hal.
-*/
-
-
-
-
-
 
 
 
@@ -93,16 +31,17 @@ void spi1_port_setup(void)
 {
   printf("spi1 port setup\n");
 
-  /* port, not driver.
-    performed once only.
+  /* device
   */
 
 	// hold cs lines lo - to put fpga in reset, avoid isolator/fpga contention, because fpga wants to become spi master and drive spi lines.
   // probably ok, if just SS held lo.
   // there's a delay of about 1ms. though before ice40 samples inputs.
 
-  assert(0);  // these are no longer in scope.
-              // but we can enforce order  in main.c
+  assert(0);  
+
+  // these are no longer in scope.
+  // but we can enforce order  in main.c
 	// spi_port_cs2_enable( SPI1);
   // spi_port_cs1_enable( SPI1);
 
@@ -175,6 +114,79 @@ void spi1_port_setup(void)
 }
 
 
+
+
+/*
+  this interupt can be for any kind of status change notice
+  so, it's better to make a generalized func, rather than push this
+  code into the ads131 code.
+*/
+
+static void (*spi1_interupt)(void *ctx) = NULL;
+static void *spi1_ctx = NULL;
+
+void spi1_port_interupt_handler_set( void (*f)(void *), void *ctx)
+{
+  spi1_interupt = f;
+  spi1_ctx = ctx;
+}
+
+
+/*
+  mar. 2024.  interuupt moved from PA2 to PA3.
+
+*/
+
+void exti3_isr(void) // called by runtime
+{
+  /*
+    OK. bizarre. resetting immediately, prevents being called a second time
+  */
+  exti_reset_request(EXTI3);
+
+  if(spi1_interupt) {
+    spi1_interupt(spi1_ctx);
+  }
+}
+
+
+
+
+
+void spi1_port_interupt_setup()
+{
+  gpio_mode_setup(SPI1_PORT, GPIO_MODE_INPUT, GPIO_PUPD_NONE, SPI1_INTERUPT);
+
+  // ie. use exti2 for pa2, exti3 for pa3
+  nvic_enable_irq(NVIC_EXTI3_IRQ);
+  // nvic_set_priority(NVIC_EXTI3_IRQ, 5 );
+
+  exti_select_source(EXTI3, SPI1_PORT);
+  exti_set_trigger(EXTI3 , EXTI_TRIGGER_RISING );         // JA. nov 1. 2023. to make consistent with _valid signal hi.
+  exti_enable_request(EXTI3);
+}
+
+
+
+
+//////////////////////////////
+
+
+//////////////////////////
+
+
+
+/*
+  jan 2024.
+  - if put all these in a data structure - then the enable/disable could be made more generic. for spi1,or spi2 etc.
+  - we would'nt have to carry around the spi integer. in app.
+  - not sure the callback works.
+  ----
+  - rather than having to prefix with spi1... everywhere.
+
+*/
+
+// For dsn257.
 
 
 #if 0
@@ -251,122 +263,30 @@ bool spi_port_cdone_get(void)
 //////////////////
 
 
-/*
-  this interupt can be for any kind of status change notice
-  so, it's better to make a generalized func, rather than push this
-  code into the ads131 code.
-*/
-
-static void (*spi1_interupt)(void *ctx) = NULL;
-static void *spi1_ctx = NULL;
-
-void spi1_port_interupt_handler_set( void (*f)(void *), void *ctx)
-{
-  spi1_interupt = f;
-  spi1_ctx = ctx;
-}
-
 
 /*
-  mar. 2024.  interuupt moved from PA2 to PA3.
+  feb 2024. splitting spi configuration over two ports (mosi on PB5) . is messy. but needed to free PA7 pin for ethernet.
+  keep the cs1 and cs2 gpio config separate
 
+
+  see hal.h  and PIN() macro for cleaner example looping an array.
+    vim ~/devel/mongoose/examples/stm32/nucleo-f429zi-make-baremetal-builtin/hal.
+
+
+   consider file rename spi1-port.c
+    because it is specific.
+
+  ---
+  - this code is just about generic enough. to move to lib2
+  - perhaps should make generic for any spi.  and/or  prefix filename spi1-port.c  etc.
+
+  ---
+  anything through a 6 pin adum/cap isolator
+
+  Code is shared for smu and adc
+  move to shared library?
+
+ 
 */
-
-void exti3_isr(void) // called by runtime
-{
-  /*
-    OK. bizarre. resetting immediately, prevents being called a second time
-  */
-  exti_reset_request(EXTI3);
-
-  if(spi1_interupt) {
-    spi1_interupt(spi1_ctx);
-  }
-}
-
-
-
-
-
-void spi1_port_interupt_setup()
-{
-  gpio_mode_setup(SPI1_PORT, GPIO_MODE_INPUT, GPIO_PUPD_NONE, SPI1_INTERUPT);
-
-  // ie. use exti2 for pa2, exti3 for pa3
-  nvic_enable_irq(NVIC_EXTI3_IRQ);
-  // nvic_set_priority(NVIC_EXTI3_IRQ, 5 );
-
-  exti_select_source(EXTI3, SPI1_PORT);
-  exti_set_trigger(EXTI3 , EXTI_TRIGGER_RISING );         // JA. nov 1. 2023. to make consistent with _valid signal hi.
-  exti_enable_request(EXTI3);
-}
-
-
-
-
-//////////////////////////////
-
-
-
-
-// with all the enable/disable.
-// probably better. to have  a  cs_set( val )   // lo,hi.
-
-
-void spi2_port_setup(void)
-{
-  printf("spi2 port setup\n");
-
-  // rcc_periph_clock_enable(RCC_SPI2);
-
-
-  // spi2.  oct 2024.
-  // clk. PB10.
-  // miso PB14
-  // mosi PB15
-
-  gpio_mode_setup(GPIOB, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO10  | GPIO14 | GPIO15  );    // clk/miso/mosi  PB10,PB14,PB15
-  gpio_set_af(GPIOB, GPIO_AF5, GPIO10 | GPIO14 | GPIO15);       // clk/miso/mosi
-  gpio_set_output_options(GPIOB, GPIO_OTYPE_PP, GPIO_OSPEED_50MHZ, GPIO10 | GPIO15);   // clk, mosi   PB10,PB15
-
-
-// for some reason this fucks up ability to read spi...
-//  gpio_mode_setup(GPIOB, GPIO_MODE_INPUT, GPIO_PUPD_NONE, GPIO14)     ;   // miso PB14.   default
-
-
-  printf("spi2 port setup done\n");
-
-  // as well as more than one interupt. may get more
-
-#if 0
-
-  // creset pc6
-  // CS_u509 / pc7
-  gpio_mode_setup( GPIOC, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE,  GPIO7);      // PC0,PC6, PC7
-  gpio_set_output_options(GPIOC, GPIO_OTYPE_PP, GPIO_OSPEED_50MHZ,  | GPIO7 );
-
-
-  // CS_u704 / pe1
-  gpio_mode_setup( GPIOE, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE,  GPIO1);              // PE1
-  gpio_set_output_options(GPIOE, GPIO_OTYPE_PP, GPIO_OSPEED_50MHZ,  GPIO1 );
-
-  // CS_spt2046 / pb9
-  gpio_mode_setup( GPIOB, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE,  GPIO9);              // PB9
-  gpio_set_output_options(GPIOB, GPIO_OTYPE_PP, GPIO_OSPEED_50MHZ,  GPIO9 );
-
-
-  // spi2_u202_int  pc2
-  // spi2_u509_int   pc8.
-  // spi2_xpt2046   pb4.
-
-  // need reset/cdone.    perhaps spi_port_creset_u202().
-  // bool spi_port_cdone_u202_get(void)
-
-
-  // input
-
-#endif
-}
-
 
 
