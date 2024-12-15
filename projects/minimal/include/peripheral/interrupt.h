77c318ea3cdea30cd60930785d45d5390a3c2547
julian1 <git@julian1.io>
2024-12-16 08:38:50 +1100

    interupt peripheral tidy

--- projects/minimal/include/peripheral/interrupt.h
+++ projects/minimal/include/peripheral/interrupt.h
@@ -1,31 +1,45 @@
 
 #pragma once
 
-// peripheral interface/abstraction
+/* peripheral interface/abstraction
 // basic interrupt should be abstract, and un-associated with any device.
 
 
-// we should try to put the systick interupt on this structure also
+// should try to put the systick interupt on this structure also
+*/
 
 typedef struct interrupt_t  interrupt_t;
 
 
 
-typedef   void (*interupt_handler_t)(void *ctx, interrupt_t *)   ;
+typedef void (*interupt_handler_t)(void *ctx, void *arg);
 
 
 struct interrupt_t
 {
-
-  void (*setup)( interrupt_t *);
-
   /* user settable - better to use a setter func to hide state
     but ok for now */
 
   interupt_handler_t  handler;
   void *ctx;
+
+  void (*setup)( interrupt_t *);
+  void (*set_handler)( interrupt_t *, void *ctx, interupt_handler_t);
 };
 
 
 
+static inline void interrupt_setup( interrupt_t *i)
+{
+  // assert(i);
+  i->setup( i);
+}
+
+
+
+static inline  void set_handler( interrupt_t *i, void *ctx, interupt_handler_t h)
+{
+  // assert(i);
+  i->set_handler( i, ctx, h );
+}
 
