2eb047adff28951dc15dc1cbe2774334c302f901
julian1 <git@julian1.io>
2026-02-22 19:36:30 +1100

    squash update

--- projects/minimal/src/test/test52.c
+++ projects/minimal/src/test/test52.c
@@ -141,21 +141,21 @@ static void test( app_t *app)
     app_trigger( app, false);
 
 
-    double mean_pos   = mean(   pos_values, ARRAY_SIZE(pos_values));
-    double mean_neg   = mean(   neg_values, ARRAY_SIZE(neg_values));
+    double pos_mean   = mean(   pos_values, ARRAY_SIZE(pos_values));
+    double neg_mean   = mean(   neg_values, ARRAY_SIZE(neg_values));
     double stddev_pos = stddev( pos_values, ARRAY_SIZE(pos_values));
     double stddev_neg = stddev( neg_values, ARRAY_SIZE(neg_values));
 
-    printf( "pos mean   %s\n",  str_format_float_with_commas(buf, 100, 9, mean_pos));
-    printf( "pos stddev %s\n",  str_format_float_with_commas(buf, 100, 9, stddev_pos));
-    printf( "neg mean   %s\n",  str_format_float_with_commas(buf, 100, 9, mean_neg));
-    printf( "neg stddev %s\n",  str_format_float_with_commas(buf, 100, 9, stddev_neg));
+    printf( "pos mean   %.3f, ", pos_mean);
+    printf( "stddev %.3f, ", stddev_pos);
+    printf("\n");
 
-    // printf( "aperture   %lu\n",  w_clk_count_aperture);
-
+    printf( "neg mean   %.3f, ", neg_mean);
+    printf( "stddev %.3f, ", stddev_neg);
+    printf("\n");
 
     // set w
-    w =  mean_pos / mean_neg;
+    w =  pos_mean / neg_mean;
 
   }
   // sampling off
@@ -185,6 +185,7 @@ static void test( app_t *app)
 
   // better name. count divisor/ factor.
   double values[ 10 ];
+  memset(values, 0, sizeof(values));
 
   {
     // nplc
@@ -261,14 +262,20 @@ static void test( app_t *app)
     // printf( "w_clk_count_aperture %lu\n", w_clk_count_aperture);
   }
 
-  double mean_values = mean(   values, ARRAY_SIZE(values));
-  // printf( "mean_values %s\n", str_format_float_with_commas(buf, 100, 9, mean_values));
-  printf( "mean_values %.3f\n",  mean_values);
+
+  double values_mean = mean(   values, ARRAY_SIZE(values));
+  double values_stddev = stddev( values, ARRAY_SIZE(values));
+
+  printf( "mean   %.3f, ", values_mean );
+  printf( "stddev %.3f, ", values_stddev);
+  printf("\n");
+
 
 
 
   ////////////////////////
 
+  memset(values, 0, sizeof(values));
 
     // mode_ch2_set_ref( mode);
     // mode_ch2_set_ref_lo( mode);
@@ -287,7 +294,7 @@ static void test( app_t *app)
     app_trigger( app, true);
 
     // take obs loop
-    for(unsigned i = 0; i < 5 ;)
+    for(unsigned i = 0; i < ARRAY_SIZE( values);)
     {
       printf("i %u, ", i);
 
@@ -324,20 +331,16 @@ static void test( app_t *app)
 
         printf("v %f, ", v );
 
-        double v2 = v / mean_values * 7.1 ;  //  need to adjust for the cal voltage
+        double v2 = v / values_mean * 7.1 ;  //  need to adjust for the cal voltage
 
         printf( "v2 %s, ", str_format_float_with_commas(buf, 100, 9, v2));
         // printf("v2 %f, ", v2 );
 
+        values[i] = v2;
         ++i;
-
       }
       else
         assert(0);
-
-
-      // ahhh no. we have to repeat the hi lo. thing going.
-      // double v = clk_count_refmux_pos -   w_clk_count_refmux
 
 
       printf("\n");
@@ -345,19 +348,22 @@ static void test( app_t *app)
     // trig off
     app_trigger( app, false);
 
-    // normal sample acquisition/adc operation
-    mode_reg_cr_set( mode, MODE_DIRECT);
 
+    // rename values_mean
+    values_mean = mean(   values, ARRAY_SIZE(values));
+    values_stddev = stddev( values, ARRAY_SIZE(values));
 
-    app_transition_state( app);
-
-
-
+    printf( "mean   %s, ", str_format_float_with_commas(buf, 100, 9, values_mean));
+    printf( "stddev %s, ", str_format_float_with_commas(buf, 100, 9, values_stddev));
     printf("\n");
 
 
+    // switch back to direct mode operation
+    mode_reg_cr_set( mode, MODE_DIRECT);
 
+    app_transition_state( app);
 
+    printf("\n");
 }
 
 
@@ -382,56 +388,64 @@ bool app_test52(
 
 #if 0
 
-
 > test52
 test52()
 set amp gain
-i 0,   first=1  idx=0 seq_n=2, counts pos 1975922 neg 2025233, sigmux       0,
-i 1,   first=0  idx=1 seq_n=2, counts pos 1975926 neg 2025237, sigmux       0,
-i 2,   first=0  idx=0 seq_n=2, counts pos 1975924 neg 2025235, sigmux       0,
-i 3,   first=0  idx=1 seq_n=2, counts pos 1975926 neg 2025237, sigmux       0,
-i 4,   first=0  idx=0 seq_n=2, counts pos 1975921 neg 2025232, sigmux       0,
-i 5,   first=0  idx=1 seq_n=2, counts pos 1975925 neg 2025236, sigmux       0,
-i 6,   first=0  idx=0 seq_n=2, counts pos 1975924 neg 2025235, sigmux       0,
-i 7,   first=0  idx=1 seq_n=2, counts pos 1975924 neg 2025235, sigmux       0,
-i 8,   first=0  idx=0 seq_n=2, counts pos 1975922 neg 2025233, sigmux       0,
-i 9,   first=0  idx=1 seq_n=2, counts pos 1975924 neg 2025235, sigmux       0,
-pos mean   1975923.800,000,000,
-pos stddev 1.600,000,000,
-neg mean   2025234.800,000,000,
-neg stddev 1.600,000,000,
-w 0.975,651,712,
-i 0, first=1  idx=0 seq_n=2, counts pos 1976363 neg 2025633, sigmux 4000001,
-i 0, first=0  idx=1 seq_n=2, counts pos  988741 neg 3013361, sigmux 4000001, v -1951300.514277,
-i 1, first=0  idx=0 seq_n=2, counts pos 1976362 neg 2025632, sigmux 4000001,
-i 1, first=0  idx=1 seq_n=2, counts pos  988741 neg 3013361, sigmux 4000001, v -1951300.489929,
-i 2, first=0  idx=0 seq_n=2, counts pos 1976361 neg 2025631, sigmux 4000001,
-i 2, first=0  idx=1 seq_n=2, counts pos  988741 neg 3013361, sigmux 4000001, v -1951300.465581,
-i 3, first=0  idx=0 seq_n=2, counts pos 1976357 neg 2025627, sigmux 4000001,
-i 3, first=0  idx=1 seq_n=2, counts pos  988740 neg 3013360, sigmux 4000001, v -1951300.392536,
-i 4, first=0  idx=0 seq_n=2, counts pos 1976357 neg 2025627, sigmux 4000001,
-i 4, first=0  idx=1 seq_n=2, counts pos  988736 neg 3013356, sigmux 4000001, v -1951300.489929,
-i 5, first=0  idx=0 seq_n=2, counts pos 1976357 neg 2025627, sigmux 4000001,
-i 5, first=0  idx=1 seq_n=2, counts pos  988736 neg 3013356, sigmux 4000001, v -1951300.489929,
-i 6, first=0  idx=0 seq_n=2, counts pos 1976359 neg 2025629, sigmux 4000001,
-i 6, first=0  idx=1 seq_n=2, counts pos  988738 neg 3013358, sigmux 4000001, v -1951300.489929,
-i 7, first=0  idx=0 seq_n=2, counts pos 1976362 neg 2025632, sigmux 4000001,
-i 7, first=0  idx=1 seq_n=2, counts pos  988742 neg 3013362, sigmux 4000001, v -1951300.465581,
-i 8, first=0  idx=0 seq_n=2, counts pos 1976365 neg 2025635, sigmux 4000001,
-i 8, first=0  idx=1 seq_n=2, counts pos  988744 neg 3013364, sigmux 4000001, v -1951300.489929,
-i 9, first=0  idx=0 seq_n=2, counts pos 1976367 neg 2025637, sigmux 4000001,
-i 9, first=0  idx=1 seq_n=2, counts pos  988744 neg 3013364, sigmux 4000001, v -1951300.538625,
-mean_values -1951300.483
-i 0, first=1  idx=0 seq_n=2, counts pos 1976371 neg 2025641, sigmux 4000001,
-i 0, first=0  idx=1 seq_n=2, counts pos  988750 neg 3013370, sigmux 4000001, v -1951300.489929, v2 7.100,000,027,,
-i 1, first=0  idx=0 seq_n=2, counts pos 1976365 neg 2025635, sigmux 4000001,
-i 1, first=0  idx=1 seq_n=2, counts pos  988744 neg 3013364, sigmux 4000001, v -1951300.489929, v2 7.100,000,027,,
-i 2, first=0  idx=0 seq_n=2, counts pos 1976363 neg 2025633, sigmux 4000001,
-i 2, first=0  idx=1 seq_n=2, counts pos  988745 neg 3013365, sigmux 4000001, v -1951300.416884, v2 7.099,999,761,,
-i 3, first=0  idx=0 seq_n=2, counts pos 1976366 neg 2025636, sigmux 4000001,
-i 3, first=0  idx=1 seq_n=2, counts pos  988746 neg 3013366, sigmux 4000001, v -1951300.465581, v2 7.099,999,938,,
-i 4, first=0  idx=0 seq_n=2, counts pos 1976370 neg 2025640, sigmux 4000001,
-i 4, first=0  idx=1 seq_n=2, counts pos  988747 neg 3013367, sigmux 4000001, v -1951300.538625, v2 7.100,000,204,,
+i 0,   first=1  idx=0 seq_n=2, counts pos 1975925 neg 2025236, sigmux       0,
+i 1,   first=0  idx=1 seq_n=2, counts pos 1975927 neg 2025238, sigmux       0,
+i 2,   first=0  idx=0 seq_n=2, counts pos 1975928 neg 2025239, sigmux       0,
+i 3,   first=0  idx=1 seq_n=2, counts pos 1975929 neg 2025240, sigmux       0,
+i 4,   first=0  idx=0 seq_n=2, counts pos 1975928 neg 2025239, sigmux       0,
+i 5,   first=0  idx=1 seq_n=2, counts pos 1975931 neg 2025242, sigmux       0,
+i 6,   first=0  idx=0 seq_n=2, counts pos 1975925 neg 2025236, sigmux       0,
+i 7,   first=0  idx=1 seq_n=2, counts pos 1975928 neg 2025239, sigmux       0,
+i 8,   first=0  idx=0 seq_n=2, counts pos 1975926 neg 2025237, sigmux       0,
+i 9,   first=0  idx=1 seq_n=2, counts pos 1975928 neg 2025239, sigmux       0,
+pos mean   1975927.500, stddev 1.746,
+neg mean   2025238.500, stddev 1.746,
+w 0.975,651,757,
+i 0, first=1  idx=0 seq_n=2, counts pos 1976369 neg 2025640, sigmux 4000001,
+i 0, first=0  idx=1 seq_n=2, counts pos  988745 neg 3013366, sigmux 4000001, v -1951300.606911,
+i 1, first=0  idx=0 seq_n=2, counts pos 1976334 neg 2025604, sigmux 4000001,
+i 1, first=0  idx=1 seq_n=2, counts pos  988745 neg 3013366, sigmux 4000001, v -1951300.730374,
+i 2, first=0  idx=0 seq_n=2, counts pos 1976337 neg 2025607, sigmux 4000001,
+i 2, first=0  idx=1 seq_n=2, counts pos  988712 neg 3013332, sigmux 4000001, v -1951300.631259,
+i 3, first=0  idx=0 seq_n=2, counts pos 1976336 neg 2025606, sigmux 4000001,
+i 3, first=0  idx=1 seq_n=2, counts pos  988750 neg 3013371, sigmux 4000001, v -1951300.657329,
+i 4, first=0  idx=0 seq_n=2, counts pos 1976338 neg 2025608, sigmux 4000001,
+i 4, first=0  idx=1 seq_n=2, counts pos  988712 neg 3013332, sigmux 4000001, v -1951300.655607,
+i 5, first=0  idx=0 seq_n=2, counts pos 1976336 neg 2025606, sigmux 4000001,
+i 5, first=0  idx=1 seq_n=2, counts pos  988713 neg 3013333, sigmux 4000001, v -1951300.582562,
+i 6, first=0  idx=0 seq_n=2, counts pos 1976342 neg 2025612, sigmux 4000001,
+i 6, first=0  idx=1 seq_n=2, counts pos  988719 neg 3013339, sigmux 4000001, v -1951300.582562,
+i 7, first=0  idx=0 seq_n=2, counts pos 1976347 neg 2025617, sigmux 4000001,
+i 7, first=0  idx=1 seq_n=2, counts pos  988715 neg 3013335, sigmux 4000001, v -1951300.801697,
+i 8, first=0  idx=0 seq_n=2, counts pos 1976342 neg 2025612, sigmux 4000001,
+i 8, first=0  idx=1 seq_n=2, counts pos  988712 neg 3013332, sigmux 4000001, v -1951300.753000,
+i 9, first=0  idx=0 seq_n=2, counts pos 1976338 neg 2025608, sigmux 4000001,
+i 9, first=0  idx=1 seq_n=2, counts pos  988746 neg 3013367, sigmux 4000001, v -1951300.803419,
+mean   -1951300.680, stddev 0.081,
+i 0, first=1  idx=0 seq_n=2, counts pos 1976370 neg 2025641, sigmux 4000001,
+i 0, first=0  idx=1 seq_n=2, counts pos  988744 neg 3013365, sigmux 4000001, v -1951300.655607, v2 7.099,999,910,,
+i 1, first=0  idx=0 seq_n=2, counts pos 1976370 neg 2025641, sigmux 4000001,
+i 1, first=0  idx=1 seq_n=2, counts pos  988745 neg 3013366, sigmux 4000001, v -1951300.631259, v2 7.099,999,821,,
+i 2, first=0  idx=0 seq_n=2, counts pos 1976339 neg 2025609, sigmux 4000001,
+i 2, first=0  idx=1 seq_n=2, counts pos  988748 neg 3013369, sigmux 4000001, v -1951300.779070, v2 7.100,000,359,,
+i 3, first=0  idx=0 seq_n=2, counts pos 1976337 neg 2025607, sigmux 4000001,
+i 3, first=0  idx=1 seq_n=2, counts pos  988743 neg 3013364, sigmux 4000001, v -1951300.852115, v2 7.100,000,625,,
+i 4, first=0  idx=0 seq_n=2, counts pos 1976368 neg 2025639, sigmux 4000001,
+i 4, first=0  idx=1 seq_n=2, counts pos  988739 neg 3013360, sigmux 4000001, v -1951300.728652, v2 7.100,000,175,,
+i 5, first=0  idx=0 seq_n=2, counts pos 1976333 neg 2025603, sigmux 4000001,
+i 5, first=0  idx=1 seq_n=2, counts pos  988744 neg 3013365, sigmux 4000001, v -1951300.730374, v2 7.100,000,182,,
+i 6, first=0  idx=0 seq_n=2, counts pos 1976335 neg 2025605, sigmux 4000001,
+i 6, first=0  idx=1 seq_n=2, counts pos  988747 neg 3013368, sigmux 4000001, v -1951300.706026, v2 7.100,000,093,,
+i 7, first=0  idx=0 seq_n=2, counts pos 1976333 neg 2025603, sigmux 4000001,
+i 7, first=0  idx=1 seq_n=2, counts pos  988748 neg 3013369, sigmux 4000001, v -1951300.632981, v2 7.099,999,827,,
+i 8, first=0  idx=0 seq_n=2, counts pos 1976336 neg 2025606, sigmux 4000001,
+i 8, first=0  idx=1 seq_n=2, counts pos  988750 neg 3013371, sigmux 4000001, v -1951300.657329, v2 7.099,999,916,,
+i 9, first=0  idx=0 seq_n=2, counts pos 1976341 neg 2025611, sigmux 4000001,
+i 9, first=0  idx=1 seq_n=2, counts pos  988749 neg 3013370, sigmux 4000001, v -1951300.803419, v2 7.100,000,447,,
+mean   7.100,000,135,, stddev 0.000,000,262,,
 
 #endif
 
