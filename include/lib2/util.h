d632351335d9bccad846a8c78aa5674e6a3e3f1f
julian1 <mail@julian1.io>
2026-02-26 10:11:19 +1100

    tidy

--- include/lib2/util.h
+++ include/lib2/util.h
@@ -1,9 +1,8 @@
 /*
-  helper stuff that belongs in separate file, but not in separate library
-  because might change
-	--------
 
-  TODO better suffix name.   _init() instead of _setup()
+  now mostly only prototypes, for code that has been moved elsewhere
+  mostly into project specific
+
 */
 
 #pragma once
@@ -12,36 +11,8 @@
 extern "C" {
 #endif
 
-#if 0
-#include <stdint.h> // uint32_t
-#endif
-
-#if 0
-
-void systick_setup(uint32_t tick_divider);
-void systick_handler_set( void (*pfunc)(void *),  void *ctx);
-
-#endif
 
 
-
-/*
-  - passing system_millis explicitly is good, and makes it clear what the dependency is.
-  - could pass sleep dependent object/environment.
-  - but it doesn't matter. for single variable.
-*/
-
-
-/*
-void msleep(uint32_t delay, volatile uint32_t *system_millis );
-
-void yield_with_msleep(uint32_t delay, volatile uint32_t *system_millis,  void (*yield)(void *), void *yield_ctx  );
-*/
-
-#if 0
-void print_stack_pointer(void);
-
-#endif
 
 
 #define UNUSED(x) ((void)(x))
@@ -50,6 +21,8 @@ void print_stack_pointer(void);
 
 #define MIN(A, B) ((A) < (B) ? (A) : (B))
 #define MAX(A, B) ((A) > (B) ? (A) : (B))
+
+
 
 
 
