7d930e926a44fa95fc00dc98f9c487bae93a611c
julian1 <mail@julian1.io>
2026-03-10 10:00:49 +1100

    forgotten file

---
+++ projects/minimal/include/cal/transfer.h
@@ -0,0 +1,61 @@
+
+
+#pragma once
+
+
+#include <stdbool.h>
+
+
+typedef struct app_t app_t;
+typedef struct cal_t cal_t;
+
+// put all these in a separate header???
+
+
+
+/*
+  use reasonable names
+  we can always put these functions in an array
+  and call by index
+  ----
+
+  EXTR> may want to pass a context.
+    to mangge setting nplc. number of obs. delay. etc.
+*/
+
+
+typedef struct transfer_t
+{
+  // does this behond in app.h. functions are mostly typed on app_t here.
+  // magic
+  void (*step1)( app_t *app);
+  void (*step2)( app_t *app);
+  void (*cal_set_value)( cal_t *cal, double mean0, double mean1);
+
+} transfer_t;
+
+void app_transfer( app_t *app, transfer_t *transfer);
+
+
+// we want a repl here.
+bool app_transfer_repl_statement( app_t *app, const char *cmd);
+
+// void app_cal_setup( app_t *app);
+// void app_cal_finish( app_t *app);
+
+void app_cal_w( app_t *app);
+void app_cal_b( app_t *app);
+void app_cal_b10( app_t *app);
+void app_cal_b100( app_t *app);
+void app_cal_b1000( app_t *app);
+void app_cal_div1000( app_t *app);
+
+
+void app_cal_all( app_t *app);
+
+
+void app_fill_buffer( app_t *app, double *values, size_t n);
+void app_fill_buffer1( app_t *app, double *pos_values, double *neg_values, size_t n);
+
+
+
