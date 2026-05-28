585035b998072402eb346ad334354497c943503c
julian1 <mail@julian1.io>
2026-05-28 16:47:11 +1000

    tidy logic

--- projects/minimal/src/mode.c
+++ projects/minimal/src/mode.c
@@ -392,9 +392,8 @@ static void compile_sa_az_hi_first( sa_state_t *sa)
   assert( !sa->noaz);
 
   // clear memory
-  _Static_assert( sizeof( sa->terms) == sizeof( term_t) * 4);
+  _Static_assert( ARRAY_SIZE( sa->terms) ==  4);
   memset( sa->terms, 0, sizeof( sa->terms));
-
 
 
   bool is_ch1 = strcmp( sa->input, "ch1") == 0;
@@ -478,7 +477,7 @@ static void compile_sa_noaz_lo_first( sa_state_t *sa /* , const char *sbool noaz
   assert( sa->noaz);
 
   // clear memory
-  _Static_assert( sizeof( sa->terms) == sizeof( term_t) * 4);
+  _Static_assert( ARRAY_SIZE( sa->terms) ==  4);
   memset( sa->terms, 0, sizeof( sa->terms));
 
 
